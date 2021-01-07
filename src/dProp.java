538b3c40c7a41499c5a5160e2949ecb6accbb9a6
amanet2 <tonyjmstallion@gmail.com>
2021-01-07 04:06:53 -0600

    prop drop shadows

--- src/dProp.java
+++ src/dProp.java
@@ -1,5 +1,6 @@
 import java.awt.*;
 import java.awt.geom.AffineTransform;
+import java.awt.geom.Rectangle2D;
 
 public class dProp {
     public static void drawProps(Graphics2D g2)
@@ -33,11 +34,34 @@ public class dProp {
                         else if(prop.getInt("int0") < 1 && !prop.get("sprite").contains("flag_red"))
                             prop.setSpriteFromPath(eUtils.getPath("misc/flag_red.png"));
                     }
-                    if(prop.isInt("code", gProp.POWERUP))
+                    if(prop.isInt("code", gProp.POWERUP)) {
+                        //prop shadow
+                        if (cVars.getInt("maptype") == gMap.MAP_TOPVIEW) {
+                            if(sVars.isOne("vfxenableshadows")) {
+                                Rectangle2D shadowBounds = new Rectangle.Double(
+                                    eUtils.scaleInt(prop.getInt("coordx") - cVars.getInt("camx")),
+                                    eUtils.scaleInt(prop.getInt("coordy") - cVars.getInt("camy")
+                                            + prop.getInt("dimh")/2),
+                                    gWeapons.weapons_selection[prop.getInt("int0")].sprite.getWidth(null),
+                                gWeapons.weapons_selection[prop.getInt("int0")].sprite.getHeight(null)/4.0);
+                                if(shadowBounds.getWidth() > 0 && shadowBounds.getHeight() > 0) {
+                                    RadialGradientPaint df = new RadialGradientPaint(
+                                            shadowBounds, new float[]{0f, 1f},
+                                            new Color[]{
+                                                    new Color(0, 0, 0, cVars.getInt("vfxshadowalpha1")),
+                                                    new Color(0, 0, 0, 0)
+                                            }, MultipleGradientPaint.CycleMethod.NO_CYCLE);
+                                    g2.setPaint(df);
+                                    g2.fillRect((int) shadowBounds.getX(), (int) shadowBounds.getY(), (int) shadowBounds.getWidth(),
+                                            (int) shadowBounds.getHeight());
+                                }
+                            }
+                        }
                         g2.drawImage(gWeapons.weapons_selection[prop.getInt("int0")].sprite,
                                 eUtils.scaleInt(prop.getInt("coordx") - cVars.getInt("camx")),
                                 eUtils.scaleInt(prop.getInt("coordy") - cVars.getInt("camy")),
                                 null);
+                    }
                     else
                         g2.drawImage(prop.sprite,
                             eUtils.scaleInt(prop.getInt("coordx") - cVars.getInt("camx")),
