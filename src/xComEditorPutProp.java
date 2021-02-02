215081032e830ab9aa5fa40c44c1e6515af54457
amanet2 <tonyjmstallion@gmail.com>
2021-02-02 02:09:20 -0600

    fill in the map

--- src/xComEditorPutProp.java
+++ src/xComEditorPutProp.java
@@ -10,14 +10,6 @@ public class xComEditorPutProp extends xCom {
             int w = Integer.parseInt(toks[6]);
             int h = Integer.parseInt(toks[7]);
             switch(propcode) {
-//                TELEPORTER = 0;
-//                LADDER = 2;
-//                SCOREPOINT = 3;
-//                FLAGRED = 5;
-//                FLAGBLUE = 6;
-//                POWERUP = 7;
-//                BOOSTUP = 8;
-//                BALLBOUNCY = 9;
                 case gProp.TELEPORTER:
                     gPropTeleporter teleporter = new gPropTeleporter(int0, int1, x, y, w, h);
                     teleporter.put("id", cScripts.createID(8));
@@ -25,32 +17,54 @@ public class xComEditorPutProp extends xCom {
                             eManager.currentMap.scene.getThingMap("PROP_TELEPORTER").size());
                     teleporter.putInt("native", 1);
                     eManager.currentMap.scene.props().add(teleporter);
-                    eManager.currentMap.scene.getThingMap("PROP_TELEPORTER").put(teleporter.get("id"),
-                            teleporter);
+                    eManager.currentMap.scene.getThingMap("PROP_TELEPORTER").put(teleporter.get("id"), teleporter);
+                    break;
+                case gProp.SCOREPOINT:
+                    gPropScorepoint scorepoint = new gPropScorepoint(int0, int1, x, y, w, h);
+                    scorepoint.put("id", cScripts.createID(8));
+                    scorepoint.putInt("tag",
+                            eManager.currentMap.scene.getThingMap("PROP_SCOREPOINT").size());
+                    scorepoint.putInt("native", 1);
+                    eManager.currentMap.scene.props().add(scorepoint);
+                    eManager.currentMap.scene.getThingMap("PROP_SCOREPOINT").put(scorepoint.get("id"), scorepoint);
+                    break;
+                case gProp.FLAGRED:
+                    gPropFlagRed flagred = new gPropFlagRed(int0, int1, x, y, w, h);
+                    flagred.put("id", cScripts.createID(8));
+                    flagred.putInt("tag",
+                            eManager.currentMap.scene.getThingMap("PROP_FLAGRED").size());
+                    flagred.putInt("native", 1);
+                    eManager.currentMap.scene.props().add(flagred);
+                    eManager.currentMap.scene.getThingMap("PROP_FLAGRED").put(flagred.get("id"), flagred);
+                    break;
+                case gProp.FLAGBLUE:
+                    gPropFlagBlue flagblue = new gPropFlagBlue(int0, int1, x, y, w, h);
+                    flagblue.put("id", cScripts.createID(8));
+                    flagblue.putInt("tag",
+                            eManager.currentMap.scene.getThingMap("PROP_FLAGBLUE").size());
+                    flagblue.putInt("native", 1);
+                    eManager.currentMap.scene.props().add(flagblue);
+                    eManager.currentMap.scene.getThingMap("PROP_FLAGBLUE").put(flagblue.get("id"), flagblue);
                     break;
                 case gProp.POWERUP:
                     gPropPowerup powerup = new gPropPowerup(int0, int1, x, y, w, h);
+                    powerup.put("id", cScripts.createID(8));
                     powerup.putInt("tag", eManager.currentMap.scene.getThingMap("PROP_POWERUP").size());
                     powerup.putInt("native", 1);
                     eManager.currentMap.scene.props().add(powerup);
-                    eManager.currentMap.scene.getThingMap("PROP_POWERUP").put(cScripts.createID(8), powerup);
+                    eManager.currentMap.scene.getThingMap("PROP_POWERUP").put(powerup.get("id"), powerup);
+                    break;
+                case gProp.BOOSTUP:
+                    gPropBoostup boostup = new gPropBoostup(int0, int1, x, y, w, h);
+                    boostup.put("id", cScripts.createID(8));
+                    boostup.putInt("tag", eManager.currentMap.scene.getThingMap("PROP_BOOSTUP").size());
+                    boostup.putInt("native", 1);
+                    eManager.currentMap.scene.props().add(boostup);
+                    eManager.currentMap.scene.getThingMap("PROP_BOOSTUP").put(boostup.get("id"), boostup);
                     break;
                 default:
                     break;
             }
-
-//            gProp p = new gProp(
-//                Integer.parseInt(toks[1]),
-//                Integer.parseInt(toks[2]),
-//                Integer.parseInt(toks[3]),
-//                Integer.parseInt(toks[4]),
-//                Integer.parseInt(toks[5]),
-//                Integer.parseInt(toks[6]),
-//                Integer.parseInt(toks[7])
-//            );
-//            p.putInt("tag", eManager.currentMap.scene.props().size());
-//            eManager.currentMap.scene.props().add(p);
-
             return "";
         }
         return "usage: e_putprop <code> <int0> <int1> <x> <y> <w> <h>";
