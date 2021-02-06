8894cb421551821a170a47fa65fc31ceb800c39f
amanet2 <tonyjmstallion@gmail.com>
2021-02-06 05:40:49 -0600

    fix up maps

--- src/xComZoom.java
+++ src/xComZoom.java
@@ -1,23 +1,13 @@
 public class xComZoom extends xCom {
     public String doCommand(String fullCommand) {
         double newzoomlevel = eUtils.zoomLevel + 0.10;
-        if(newzoomlevel <= 2.0 ) {
-            eUtils.zoomLevel = newzoomlevel;
-        }
-//        int newscalelevel = eUtils.gameScale - xCon.getInt("gamescale")/48;
-//        if(newscalelevel >= xCon.getInt("gamescale")/2)
-//            eManager.changeScaleLevel(newscalelevel);
+        eUtils.zoomLevel = newzoomlevel <= 2.0 ? newzoomlevel : eUtils.zoomLevel;
         return "+zoom";
     }
 
     public String undoCommand(String fullCommand) {
         double newzoomlevel = eUtils.zoomLevel - 0.10;
-        if(newzoomlevel > 0.2) {
-            eUtils.zoomLevel = newzoomlevel;
-        }
-//        int newscalevel = eUtils.gameScale + xCon.getInt("gamescale")/48;
-//        if(newscalevel <= sVars.getInt("gamescale")*4)
-//            eManager.changeScaleLevel(newscalevel);
+        eUtils.zoomLevel = newzoomlevel > 0.2 ? newzoomlevel : eUtils.zoomLevel;
         return "-zoom";
     }
 }
