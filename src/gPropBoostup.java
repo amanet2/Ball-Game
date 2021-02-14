1b213f7d6a5d2fa56b11324db98d5de9ec63a666
amanet2 <tonyjmstallion@gmail.com>
2021-02-14 03:23:05 -0600

    better boostups

--- src/gPropBoostup.java
+++ src/gPropBoostup.java
@@ -1,8 +1,7 @@
 public class gPropBoostup extends gProp {
     public void propEffect(gPlayer p) {
-        p.putInt("mov0", 1);
-        p.putInt("mov1", 0);
-        xCon.ex(new String[]{"cv_jumping 1", "cv_inboost 1", "cv_falltime 0"});
+//        p.putInt("mov0", 1);
+        p.putInt("vel0", cVars.getInt("velocitysuperspeed"));
     }
 
     public gPropBoostup(int ux, int uy, int x, int y, int w, int h) {
