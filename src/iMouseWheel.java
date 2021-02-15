edd54ea35bb82993e1ee0c44cb43467af69e8f13
amanet2 <tonyjmstallion@gmail.com>
2021-02-15 01:03:27 -0600

    test map and mousewheel streamline

--- src/iMouseWheel.java
+++ src/iMouseWheel.java
@@ -4,31 +4,20 @@ import java.awt.event.MouseWheelListener;
 public class iMouseWheel implements MouseWheelListener {
     public void mouseWheelMoved(MouseWheelEvent e) {
         if (e.getWheelRotation() < 0) {
-            if(sVars.isOne("inconsole")) {
-                if(xCon.linesToShowStart > 0) {
-                    xCon.linesToShowStart--;
-                }
-            }
-            else if(sSettings.show_mapmaker_ui) {
+            if(sVars.isOne("inconsole"))
+                xCon.linesToShowStart = Math.max(0, xCon.linesToShowStart-1);
+            else if(sSettings.show_mapmaker_ui)
                 xCon.ex("-e_nextthing");
-            }
-            else {
+            else
                 xCon.ex("dropweapon");
-            }
         }
         else {
-            if(sVars.isOne("inconsole")) {
-                if(xCon.linesToShowStart <
-                    xCon.stringLines.size() - xCon.linesToShow) {
-                    xCon.linesToShowStart++;
-                }
-            }
-            else if(sSettings.show_mapmaker_ui) {
+            if(sVars.isOne("inconsole"))
+                xCon.linesToShowStart = Math.min(xCon.stringLines.size() - xCon.linesToShow, xCon.linesToShowStart + 1);
+            else if(sSettings.show_mapmaker_ui)
                 xCon.ex("e_nextthing");
-            }
-            else {
+            else
                 xCon.ex("dropweapon");
-            }
         }
     }
 }
