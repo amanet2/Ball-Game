0faedf7f7d9433268e4828523796ff752c027861
amanet2 <tonyjmstallion@gmail.com>
2021-03-12 23:29:23 -0600

    better new game and you can move the camera to a specific x,y upon loading a map

--- src/xComNewgame.java
+++ src/xComNewgame.java
@@ -1,8 +1,12 @@
 public class xComNewgame extends xCom {
     public String doCommand(String fullCommand) {
-        sSettings.net_server = true;
-        sSettings.NET_MODE = sSettings.NET_SERVER;
-        xCon.ex(String.format("load wp_steam_gray.map"));
+        int toplay = eManager.mapSelectionIndex;
+        if(toplay < 0) {
+            xCon.ex("newgamerandom");
+        }
+        else {
+            xCon.ex(String.format("load %s", eManager.mapsSelection[toplay]));
+        }
         return "new game started";
     }
 }
