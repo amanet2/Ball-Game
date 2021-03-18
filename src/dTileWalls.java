bdba61aaac36442907f6133ee91eef4539c6c791
amanet2 <tonyjmstallion@gmail.com>
2021-03-18 00:59:48 -0500

    floors and upper corners drawing without shading

--- src/dTileWalls.java
+++ src/dTileWalls.java
@@ -25,6 +25,75 @@ public class dTileWalls {
                     );
                 }
             }
+            squareMap = eManager.currentMap.scene.getThingMap("BLOCK_CORNERUR");
+            for(String tag : squareMap.keySet()) {
+                gBlockCornerUR block = (gBlockCornerUR) squareMap.get(tag);
+                if(block.contains("wallh")) {
+                    String[] colorvals = block.get("colorwall").split(",");
+                    g2.setColor(new Color(
+                            Integer.parseInt(colorvals[0]),
+                            Integer.parseInt(colorvals[1]),
+                            Integer.parseInt(colorvals[2]),
+                            Integer.parseInt(colorvals[3])
+                    ));
+                    Polygon pw = new Polygon(
+                            new int[]{
+                                    eUtils.scaleInt(block.getInt("coordx") - cVars.getInt("camx")),
+                                    eUtils.scaleInt(block.getInt("coordx") - cVars.getInt("camx")
+                                            + block.getInt("dimw")),
+                                    eUtils.scaleInt(block.getInt("coordx") - cVars.getInt("camx")
+                                            + block.getInt("dimw")),
+                                    eUtils.scaleInt(block.getInt("coordx") - cVars.getInt("camx"))
+                            },
+                            new int[]{
+                                    eUtils.scaleInt(block.getInt("coordy") - cVars.getInt("camy")),
+                                    eUtils.scaleInt(block.getInt("coordy") - cVars.getInt("camy")
+                                            + block.getInt("toph")),
+                                    eUtils.scaleInt(block.getInt("coordy") - cVars.getInt("camy")
+                                            + block.getInt("toph") + block.getInt("wallh")),
+                                    eUtils.scaleInt(block.getInt("coordy") - cVars.getInt("camy") + block.getInt("wallh"))
+                            },
+                            4);
+                    g2.fillPolygon(pw);
+                }
+            }
+            squareMap = eManager.currentMap.scene.getThingMap("BLOCK_CORNERUL");
+            for(String tag : squareMap.keySet()) {
+                gBlockCornerUL block = (gBlockCornerUL) squareMap.get(tag);
+                if(block.contains("wallh")) {
+                    String[] colorvals = block.get("colorwall").split(",");
+                    g2.setColor(new Color(
+                            Integer.parseInt(colorvals[0]),
+                            Integer.parseInt(colorvals[1]),
+                            Integer.parseInt(colorvals[2]),
+                            Integer.parseInt(colorvals[3])
+                    ));
+                    Polygon pw = new Polygon(
+                            new int[]{
+                                    eUtils.scaleInt(block.getInt("coordx") - cVars.getInt("camx")),
+                                    eUtils.scaleInt(block.getInt("coordx") - cVars.getInt("camx")
+                                            + block.getInt("dimw")),
+                                    eUtils.scaleInt(block.getInt("coordx") - cVars.getInt("camx")
+                                            + block.getInt("dimw")),
+                                    eUtils.scaleInt(block.getInt("coordx") - cVars.getInt("camx"))
+                            },
+                            new int[]{
+                                    eUtils.scaleInt(block.getInt("coordy") - cVars.getInt("camy")
+                                            + block.getInt("toph")),
+                                    eUtils.scaleInt(block.getInt("coordy") - cVars.getInt("camy")),
+                                    eUtils.scaleInt(block.getInt("coordy") - cVars.getInt("camy")
+                                            + block.getInt("wallh")),
+                                    eUtils.scaleInt(block.getInt("coordy") - cVars.getInt("camy")
+                                            + block.getInt("toph") + block.getInt("wallh")),
+                            },
+                            4);
+                    g2.fillPolygon(pw);
+                }
+            }
+            //
+            // --- NEW ABOVE OLD BELOW
+            //
+
             for (gTile t : eManager.currentMap.scene.tiles()) {
                 dShadows.drawTileShadows(g2, t);
                 int d6w = t.getInt("dim6w");
