2bab2fa7af71b7b3c668f79ceeedf8d0c1bd72b6
amanet2 <tonyjmstallion@gmail.com>
2021-03-20 03:54:51 -0500

    better overhang shadows

--- src/dBlockShadows.java
+++ src/dBlockShadows.java
@@ -107,7 +107,8 @@ public class dBlockShadows {
                         eUtils.scaleInt(block.getInt("dimw")),
                         eUtils.scaleInt((int)(block.getInt("wallh")*cVars.getDouble("vfxshadowfactor")))
                 );
-            } else if (block.getInt("toph") > 0) {
+            }
+            else if (block.getInt("toph") > 0) {
                 GradientPaint gradient = new GradientPaint(
                         eUtils.scaleInt(block.getInt("coordx") + block.getInt("dimw")/2
                                 - cVars.getInt("camx")),
@@ -118,14 +119,16 @@ public class dBlockShadows {
                                 - cVars.getInt("camx")),
                         eUtils.scaleInt(block.getInt("coordy") - cVars.getInt("camy")
                                 + block.getInt("dimh")
-                                + (int)(block.getInt("dimh")*cVars.getDouble("vfxshadowfactor"))),
+                                + (int)((block.getInt("dimh") - block.getInt("toph")
+                        )*cVars.getDouble("vfxshadowfactor"))),
                         new Color(0,0,0, cVars.getInt("vfxshadowalpha2")));
                 g2.setPaint(gradient);
                 g2.fillRect(eUtils.scaleInt(block.getInt("coordx")-cVars.getInt("camx")),
                         eUtils.scaleInt(block.getInt("coordy")-cVars.getInt("camy")
                                 + block.getInt("dimh")),
                         eUtils.scaleInt(block.getInt("dimw")),
-                        eUtils.scaleInt((int)(block.getInt("dimh")*cVars.getDouble("vfxshadowfactor")))
+                        eUtils.scaleInt((int)((block.getInt("dimh") - block.getInt("toph")
+                        )*cVars.getDouble("vfxshadowfactor")))
                 );
                 //
                 // 2x gradient below
