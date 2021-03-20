5bd2e813f3442e948e696466a3e3b1b6320b4c63
amanet2 <tonyjmstallion@gmail.com>
2021-03-20 02:08:05 -0500

    more intense prefab usage

--- src/gPrefabCage.java
+++ src/gPrefabCage.java
@@ -9,30 +9,25 @@ public class gPrefabCage extends gPrefab {
                 "120,120,200,255"));
         blocks.add(new gBlockCube(x + 2400, y, 1200, 1200, 150, 1050, "120,120,120,255",
                 "120,120,200,255"));
-//        blocks.add(new gBlockCube());
-//        blocks.add(new gBlockCube());
-//        blocks.add(new gBlockCube());
-//        blocks.add(new gBlockCube());
-//        blocks.add(new gBlockCube());
-//        blocks.add(new gBlockCube());
-//        blocks.add(new gBlockCube());
-//        blocks.add(new gBlockCube());
-//        blocks.add(new gBlockCube());
-//        blocks.add(new gBlockCube());
-//        blocks.add(new gBlockCube());
+        blocks.add(new gBlockCube(x + 900, y, 300, 1800, 1800, 0, "120,120,120,255",
+                "120,120,200,255"));
+        blocks.add(new gBlockCube(x + 2400, y, 300, 1800, 1800, 0, "120,120,120,255",
+                "120,120,200,255"));
+        blocks.add(new gBlockCube(x + 1200, y + 1800, 1200, 1200, 150, 0, "120,120,120,255",
+                "120,120,200,255"));
+        gBlockCornerUR columnL = new gBlockCornerUR(x + 900, y + 1800, 300, 1200, 150, 1050,
+                "120,120,120,255", "120,120,200,255");
+        columnL.put("frontwall", "1");
+        blocks.add(columnL);
+        gBlockCornerUL columnR = new gBlockCornerUL(x + 2400, y + 1800, 300, 1200, 150, 1050,
+                "120,120,120,255", "120,120,200,255");
+        columnR.put("frontwall", "1");
+        blocks.add(columnR);
+        blocks.add(new gBlockFloor(x, y + 1200, 1200, 1200, "100,100,60,255"));
+        blocks.add(new gBlockFloor(x + 1200, y + 1200, 1200, 1200, "100,100,60,255"));
+        blocks.add(new gBlockFloor(x + 2400, y + 1200, 1200, 1200, "100,100,60,255"));
+        blocks.add(new gBlockFloor(x, y + 2400, 1200, 1200, "100,100,60,255"));
+        blocks.add(new gBlockFloor(x + 1200, y + 2400, 1200, 1200, "100,100,60,255"));
+        blocks.add(new gBlockFloor(x + 2400, y + 2400, 1200, 1200, "100,100,60,255"));
     }
-//DONE    putblock BLOCK_CUBE 0 -600 1200 1200 150 1050 120,120,120,255 120,120,200,255
-//    putblock BLOCK_CUBE -1200 -600 1200 1200 150 1050 120,120,120,255 120,120,200,255
-//    putblock BLOCK_CUBE 1200 -600 1200 1200 150 1050 120,120,120,255 120,120,200,255
-//    putblock BLOCK_CUBE -300 -600 300 1800 1800 0 120,120,120,255 120,120,200,255
-//    putblock BLOCK_CUBE 1200 -600 300 1800 1800 0 120,120,120,255 120,120,200,255
-//    putblock BLOCK_CUBE 0 1200 1200 1200 150 0 120,120,120,255 120,120,200,255
-//    putblock BLOCK_CORNERUR -300 1200 300 1200 150 1050 120,120,120,255 120,120,200,255 1
-//    putblock BLOCK_CORNERUL 1200 1200 300 1200 150 1050 120,120,120,255 120,120,200,255 1
-//    putblock BLOCK_FLOOR 0 600 1200 1200 100,100,60,255
-//    putblock BLOCK_FLOOR 0 1800 1200 1200 100,100,60,255
-//    putblock BLOCK_FLOOR 1200 600 1200 1200 100,100,60,255
-//    putblock BLOCK_FLOOR 1200 1800 1200 1200 100,100,60,255
-//    putblock BLOCK_FLOOR -1200 600 1200 1200 100,100,60,255
-//    putblock BLOCK_FLOOR -1200 1800 1200 1200 100,100,60,255
 }
