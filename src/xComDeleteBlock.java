f1366e6ede2848e9e2cca7ad3b07539b9d5398a2
amanet2 <tonyjmstallion@gmail.com>
2021-03-19 23:13:08 -0500

    deleting blocks better

--- src/xComDeleteBlock.java
+++ src/xComDeleteBlock.java
@@ -1,15 +1,21 @@
 public class xComDeleteBlock extends xCom {
     public String doCommand(String fullCommand) {
         String[] toks = fullCommand.split(" ");
-        if(toks.length > 1) {
-//            ArrayList<gBlock> blocks = eManager.currentMap.scene.blocks();
-//            int tag = Integer.parseInt(toks[1]);
-//            gBlock block = blocks.get(tag);
-//            String code = gBlocks.getTitleForBlock(block);
-//            blocks.remove(block);
-//            eManager.currentMap.scene.getThingMap(code).remove(Integer.toString(tag));
-//            return "removed block tag: " + tag;
+        if(toks.length > 2) {
+            String type = toks[1];
+            String id = toks[2];
+            if(eManager.currentMap.scene.getThingMap(type).containsKey(id)) {
+                eManager.currentMap.scene.getThingMap(type).remove(id);
+                int blockIdCtr = eManager.currentMap.scene.objectIdCtrs.get(type);
+                blockIdCtr--;
+                if (blockIdCtr < 0)
+                    blockIdCtr = 0;
+                eManager.currentMap.scene.objectIdCtrs.put(type, blockIdCtr);
+                return "removed " + type + " id: " + id;
+            }
+            else
+                return "no " + type + " found for id: " + id;
         }
-        return "usage: deleteblock <tag>";
+        return "usage: deleteblock <type> <id>";
     }
 }
