2287f8c137ba07b38b3157614e9a0d5999fd11da
amanet2 <tonyjmstallion@gmail.com>
2021-04-02 02:48:18 -0500

    have the ability to drop flag from userplayer

--- src/xComDropFlagRed.java
+++ src/xComDropFlagRed.java
@@ -1,14 +1,12 @@
 public class xComDropFlagRed extends xCom {
     public String doCommand(String fullCommand) {
-        gPlayer p = cGameLogic.userPlayer();
-        if(p != null) {
-            System.out.println("DROP FLAG (RED)");
-//            String doString = String.format("putprop %d %s %s %d %d %d %d",
-//                    gProps.getTitleForCode(gProps.FLAGRED), "1", "0", p.getInt("coordx") + p.getInt("dimw"),
-//                    p.getInt("coordy") + p.getInt("dimh"), 300, 300);
-//            cVars.putInt("weaponstock"+p.get("weapon"), 0);
-//            xCon.ex(doString);
+        String[] toks = fullCommand.split(" ");
+        if (toks.length > 1) {
+            String id = toks[1];
+            gPlayer p = gScene.getPlayerById(id);
+            xCon.ex("putitem ITEM_FLAGRED " + p.get("coordx") + " " + p.get("coordy"));
         }
-        return "drop weapon";
+
+        return "dropped flagred";
     }
 }
