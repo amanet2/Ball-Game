87d59429ba73580a5ba0ecfcf3d4cd3db1bfc2c8
amanet2 <tonyjmstallion@gmail.com>
2021-05-09 04:18:26 -0500

    confirmed no overlap in serverargs

--- src/xComAttack.java
+++ src/xComAttack.java
@@ -7,15 +7,6 @@ public class xComAttack extends xCom {
             || cVars.getInt("weaponstock"+playerWeapon) > 0) {
             if(br.getLong("cooldown") < System.currentTimeMillis()) {
                 String fireString = "fireweapon " + br.get("id") + " " + playerWeapon;
-                boolean isflagmaster = false;
-                if(cVars.isInt("gamemode", cGameMode.FLAG_MASTER) && nServer.instance().clientArgsMap != null
-                        && nServer.instance().clientArgsMap.containsKey("server")
-                        && nServer.instance().clientArgsMap.get("server").containsKey("state")
-                        && nServer.instance().clientArgsMap.get("server").get("state").contains(uiInterface.uuid)) {
-                    isflagmaster = true;
-                }
-                if(isflagmaster)
-                    return "can't attack while flagmaster";
                 cGameLogic.doCommand(fireString);
                 br.putLong("cooldown", System.currentTimeMillis()
                         + (long)(gWeapons.fromCode(br.getInt("weapon")).refiredelay));
