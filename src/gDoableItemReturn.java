037cdcbe4a45d8a4da2db53292296f2ae455eb48
amanet2 <tonyjmstallion@gmail.com>
2021-05-13 19:46:31 -0500

    itemreturn delegate

--- src/gDoableItemReturn.java
+++ src/gDoableItemReturn.java
@@ -3,23 +3,18 @@ public class gDoableItemReturn {
         return null;
     }
     public void storeItem(gItem itemToLoad) {
-        if(sSettings.IS_SERVER) {
-            gScene sceneToStore = cServerLogic.scene;
-            int itemId = sceneToStore.itemIdCtr;
-            itemToLoad.putInt("id", itemId);
-            itemToLoad.putInt("itemid", itemId);
-            sceneToStore.getThingMap("THING_ITEM").put(Integer.toString(itemId), itemToLoad);
-            sceneToStore.getThingMap(itemToLoad.get("type")).put(Integer.toString(itemId), itemToLoad);
-            sceneToStore.itemIdCtr += 1;
-        }
-        if(sSettings.IS_CLIENT) {
-            gScene sceneToStore = cClientLogic.scene;
-            int itemId = sceneToStore.itemIdCtr;
-            itemToLoad.putInt("id", itemId);
-            itemToLoad.putInt("itemid", itemId);
-            sceneToStore.getThingMap("THING_ITEM").put(Integer.toString(itemId), itemToLoad);
-            sceneToStore.getThingMap(itemToLoad.get("type")).put(Integer.toString(itemId), itemToLoad);
-            sceneToStore.itemIdCtr += 1;
-        }
+        if(sSettings.IS_SERVER)
+            storeItemDelegate(itemToLoad, cServerLogic.scene);
+        if(sSettings.IS_CLIENT)
+            storeItemDelegate(itemToLoad, cClientLogic.scene);
+    }
+
+    public void storeItemDelegate(gItem itemToLoad, gScene sceneToStore) {
+        int itemId = sceneToStore.itemIdCtr;
+        itemToLoad.putInt("id", itemId);
+        itemToLoad.putInt("itemid", itemId);
+        sceneToStore.getThingMap("THING_ITEM").put(Integer.toString(itemId), itemToLoad);
+        sceneToStore.getThingMap(itemToLoad.get("type")).put(Integer.toString(itemId), itemToLoad);
+        sceneToStore.itemIdCtr += 1;
     }
 }
