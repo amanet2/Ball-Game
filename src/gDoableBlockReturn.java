023d84cdf822fad0ad6d7b97449de188c49c41f1
amanet2 <tonyjmstallion@gmail.com>
2021-05-15 18:52:15 -0500

    touchup blockreturn

--- src/gDoableBlockReturn.java
+++ src/gDoableBlockReturn.java
@@ -2,17 +2,4 @@ public class gDoableBlockReturn {
     public gBlock getBlock(String[] args) {
         return null;
     }
-    public void storeBlock(gBlock blockToLoad) {
-        if(sSettings.IS_SERVER)
-            storeBlockDelegate(blockToLoad, cServerLogic.scene);
-//        if(sSettings.IS_CLIENT)
-//            storeBlockDelegate(blockToLoad, cClientLogic.scene);
-    }
-
-    private void storeBlockDelegate(gBlock blockToLoad, gScene sceneToStore) {
-        int blockId = sceneToStore.blockIdCtr;
-        sceneToStore.getThingMap("THING_BLOCK").put(Integer.toString(blockId), blockToLoad);
-        sceneToStore.getThingMap(blockToLoad.get("type")).put(Integer.toString(blockId), blockToLoad);
-        sceneToStore.blockIdCtr += 1;
-    }
 }
