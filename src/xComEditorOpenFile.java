f98b38d4caa7387f67d25734ad4949e294d24b61
amanet2 <tonyjmstallion@gmail.com>
2021-05-16 01:52:28 -0500

    open file'

--- src/xComEditorOpenFile.java
+++ src/xComEditorOpenFile.java
@@ -11,6 +11,9 @@ public class xComEditorOpenFile extends xCom {
                 if(xCon.instance().getInt("e_showlossalert") > 0)
                     return "";
                 File file = fileChooser.getSelectedFile();
+                if(!nServer.instance().isAlive())
+                    xCon.ex("startserver;load;joingame localhost 5555");
+
                 xCon.ex("changemap " + file.getPath());
                 uiEditorMenus.refreshGametypeCheckBoxMenuItems();
                 return "opening " + file.getPath();
