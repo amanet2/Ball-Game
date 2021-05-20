c006a2f171f4a318667f6d7581a4324d2b16b31d
amanet2 <tonyjmstallion@gmail.com>
2021-05-19 22:06:05 -0500

    new netmap without the @ symbol

--- src/nVars.java
+++ src/nVars.java
@@ -11,4 +11,23 @@ public class nVars {
         }
         return  toReturn;
     }
+
+    public static HashMap<String, HashMap<String, String>> getMapFromNetMapString(String argload) {
+        HashMap<String, HashMap<String, String>> toReturn = new HashMap<>();
+        String modstr = argload.substring(1,argload.length()-1);
+        String[] idsplit = modstr.split("},");
+        for(String s : idsplit) {
+            int ind = s.indexOf("=\\{");
+            String idtok = s;
+            if(ind > -1)
+                idtok = s.substring(0,ind);
+            toReturn.put(idtok, new HashMap<>());
+            String argstr = s.replace(idtok+"=","").substring(1,s.length()-1);
+            for(String pair : argstr.split(",")) {
+                String[] vals = pair.split("=");
+                toReturn.get(idtok).put(vals[0].trim(), vals.length > 1 ? vals[1].trim() : "");
+            }
+        }
+        return toReturn;
+    }
 }
