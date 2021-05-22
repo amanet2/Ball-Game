0ce4c94200a7b195721bcc01b75bf6055247d0f8
amanet2 <tonyjmstallion@gmail.com>
2021-05-21 23:49:02 -0500

    beautiful flares from telporters

--- src/xComEditorShowLossAlert.java
+++ src/xComEditorShowLossAlert.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 
 public class xComEditorShowLossAlert extends xCom {
     public  String doCommand(String fullcomm) {
+//        xCon.ex(String.format("playsound %s", Math.random() > 0.5 ? "sounds/shout.wav" : "sounds/death.wav"));
         if (JOptionPane.showConfirmDialog(oDisplay.instance(), "Any unsaved changes will be lost...",
                 "Are You Sure?", JOptionPane.YES_NO_OPTION) > 0)
             return "1";
