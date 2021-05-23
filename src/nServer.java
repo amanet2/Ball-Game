364fd2bd8e3609c4f2688d909099350fa23a7598
amanet2 <tonyjmstallion@gmail.com>
2021-05-23 01:46:11 -0500

    even more savings

--- src/nServer.java
+++ src/nServer.java
@@ -305,7 +305,8 @@ public class nServer extends Thread {
             workingMap.remove("time"); //unnecessary args for sending, but necessary to retain server-side
             workingMap.remove("respawntime"); //unnecessary args for sending, but necessary to retain server-side
             workingMap.remove("id"); //unnecessary args for sending, but necessary to retain server-side
-            sendDataMap.put(idload2, new HashMap<>(workingMap));
+            if(sendfull || workingMap.size() > 0)
+                sendDataMap.put(idload2, new HashMap<>(workingMap));
             sendArgsMaps.get(clientid).put(idload2, new HashMap<>(clientArgsMap.get(idload2)));
             sendArgsMaps.get(clientid).get(idload2).remove("cmdrcv");
         }
