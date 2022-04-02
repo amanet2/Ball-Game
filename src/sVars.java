9194ae5a3f1513607399ace8d1e0fe2cddf2a2e8
amanet2 <tonyjmstallion@gmail.com>
2022-04-02 03:15:10 -0500

    remove unused

--- src/sVars.java
+++ src/sVars.java
@@ -3,20 +3,9 @@ import java.util.HashMap;
 public class sVars {
     private static HashMap<String, String> keys = null;
 
-    private static void init() {
-    }
-
-    public static String get(String s) {
-        refresh();
-        return keys.get(s);
-    }
-
     public static int getInt(String s) {
-        return Integer.parseInt(get(s));
-    }
-
-    public static double getDouble(String s) {
-        return Double.parseDouble(get(s));
+        refresh();
+        return Integer.parseInt(keys.get(s));
     }
 
     public static void put(String s, String v) {
@@ -24,32 +13,9 @@ public class sVars {
         keys.put(s,v);
     }
 
-    public static void putInt(String s, int v) {
-        put(s, Integer.toString(v));
-    }
-
-    public static void putLong(String s, long v) {
-        put(s, Long.toString(v));
-    }
-
-    public static HashMap<String, String> vars() {
-        refresh();
-        return keys;
-    }
-
-    public static boolean contains(String s) {
-        return keys.containsKey(s);
-    }
-
     private static void refresh() {
         if(keys == null) {
             keys = new HashMap<>();
-            init();
         }
-    }
-
-    public static void remove(String s) {
-        refresh();
-        keys.remove(s);
     }
 }
