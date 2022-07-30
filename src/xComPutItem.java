91206f4f71dbf6936f6aa30d897c938753f0d1f3
amanet2 <tonyjmstallion@gmail.com>
2022-07-30 02:21:29 -0500

    new putitem comm

--- src/xComPutItem.java
+++ src/xComPutItem.java
@@ -1,16 +1,10 @@
-import java.util.Arrays;
-
 public class xComPutItem extends xCom {
     public String doCommand(String fullCommand) {
         String[] toks = fullCommand.split(" ");
         if(toks.length < 5)
-            return "usage: putitem <ITEM_TITLE> <id> <x> <y>";
+            return escape();
         gItemFactory factory = gItemFactory.instance();
         String itemTitle = toks[1];
-        System.out.println(itemTitle);
-        System.out.println(factory.itemLoadMap.toString());
-        System.out.println(factory.itemLoadMap.keySet());
-        System.out.println(factory.itemLoadMap.containsKey(itemTitle));
         if(factory.itemLoadMap.containsKey(itemTitle)) {
             String itemId = toks[2];
             String putX = toks[3];
@@ -20,7 +14,14 @@ public class xComPutItem extends xCom {
             newItem.put("id", itemId);
             cServerLogic.scene.getThingMap("THING_ITEM").put(itemId, newItem);
             cServerLogic.scene.getThingMap(newItem.get("type")).put(itemId, newItem);
+            nServer.instance().addExcludingNetCmd("server", "cl_"+fullCommand);
+            return "put item";
         }
-        return "usage: putitem <ITEM_TITLE> <args>";
+        return escape();
+    }
+
+    private String escape() {
+        return "usage: putitem <ITEM_TITLE> <id> <x> <y>";
+
     }
 }
