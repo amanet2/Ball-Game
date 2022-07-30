92fb31fd2e4834f7ee1deb3878e07193f839c4f0
amanet2 <tonyjmstallion@gmail.com>
2022-07-30 02:05:26 -0500

    server side works but client method doesnt

--- src/gItemPointGiver.java
+++ src/gItemPointGiver.java
@@ -6,7 +6,7 @@ public class gItemPointGiver extends gItem {
     }
 
     public gItemPointGiver(int x, int y) {
-        super(x, y, 200, 300);
+        super(x, y, 300, 300);
         put("type", "ITEM_POINTGIVER");
         sprite = gTextures.getGScaledImage(eUtils.getPath("misc/light1.png"),
                 getInt("dimw"), getInt("dimh"));
--- src/xComPutItem.java
+++ src/xComPutItem.java
@@ -1,23 +1,25 @@
+import java.util.Arrays;
+
 public class xComPutItem extends xCom {
     public String doCommand(String fullCommand) {
         String[] toks = fullCommand.split(" ");
-        if(toks.length > 2) {
-            gItemFactory factory = gItemFactory.instance();
-            String itemTitle = toks[1];
-            if(factory.itemLoadMap.containsKey(itemTitle)) {
-                gDoableItemReturn itemReturn = factory.itemLoadMap.get(itemTitle);
-                String argString = fullCommand.replace(toks[0] + " ",
-                        "").replace(toks[1] + " ", "");
-                String[] args = argString.split(" ");
-                gItem newItem = itemReturn.getItem(args);
-                newItem.putInt("itemid", cClientLogic.itemId);
-                int itemId = cServerLogic.scene.itemIdCtr;
-                newItem.putInt("id", itemId);
-                newItem.putInt("itemid", itemId);
-                cServerLogic.scene.getThingMap("THING_ITEM").put(Integer.toString(itemId), newItem);
-                cServerLogic.scene.getThingMap(newItem.get("type")).put(Integer.toString(itemId), newItem);
-                cServerLogic.scene.itemIdCtr += 1;
-            }
+        if(toks.length < 5)
+            return "usage: putitem <ITEM_TITLE> <id> <x> <y>";
+        gItemFactory factory = gItemFactory.instance();
+        String itemTitle = toks[1];
+        System.out.println(itemTitle);
+        System.out.println(factory.itemLoadMap.toString());
+        System.out.println(factory.itemLoadMap.keySet());
+        System.out.println(factory.itemLoadMap.containsKey(itemTitle));
+        if(factory.itemLoadMap.containsKey(itemTitle)) {
+            String itemId = toks[2];
+            String putX = toks[3];
+            String putY = toks[4];
+            gDoableItemReturn itemReturn = factory.itemLoadMap.get(itemTitle);
+            gItem newItem = itemReturn.getItem(new String[]{putX, putY}); //expects x y as args
+            newItem.put("id", itemId);
+            cServerLogic.scene.getThingMap("THING_ITEM").put(itemId, newItem);
+            cServerLogic.scene.getThingMap(newItem.get("type")).put(itemId, newItem);
         }
         return "usage: putitem <ITEM_TITLE> <args>";
     }
