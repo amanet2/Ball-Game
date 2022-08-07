f706017208d27840e882aaf6e5fc01b7bed400fb
amanet2 <tonyjmstallion@gmail.com>
2022-08-07 12:21:20 -0500

    some stuff

--- src/xComSetThing.java
+++ src/xComSetThing.java
@@ -3,21 +3,35 @@ import java.util.HashMap;
 public class xComSetThing extends xCom {
     public String doCommand(String fullCommand) {
         String[] toks = fullCommand.split(" ");
-        if(toks.length < 4)
+        if(toks.length < 2)
             return "null";
         String ttype = toks[1];
-        String tid = toks[2];
-        String tk = toks[3];
         if(cServerLogic.scene.getThingMap(ttype) == null)
             return "null";
         HashMap<String, gThing> thingMap = cServerLogic.scene.getThingMap(ttype);
+        if(toks.length < 3)
+            return thingMap.toString();
+        String tid = toks[2];
         if(!thingMap.containsKey(tid))
             return "null";
         gThing thing = thingMap.get(tid);
-        if(toks.length > 4)
-            thing.put(tk, toks[4]);
+        if(toks.length < 4)
+            return thing.toString();
+        String tk = toks[3];
         if(thing.get(tk) == null)
             return "null";
+        if(toks.length < 5)
+            return thing.get(tk);
+        StringBuilder tvb = new StringBuilder();
+        for(int i = 4; i < toks.length; i++) {
+            tvb.append(" ").append(toks[i]);
+        }
+        String tv = tvb.substring(1);
+        String tvr = xCon.ex("setthing " + tv);
+        if(!tvr.equals("null"))
+            thing.put(tk, tvr);
+        else
+            thing.put(tk, tv);
         return thing.get(tk);
     }
 }
