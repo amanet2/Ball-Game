e143766d23057415198c17d2a3c0a16fc4197a1c
amanet2 <tonyjmstallion@gmail.com>
2022-08-07 12:42:43 -0500

    put it back

--- src/xComSetThing.java
+++ src/xComSetThing.java
@@ -3,43 +3,27 @@ import java.util.HashMap;
 public class xComSetThing extends xCom {
     public String doCommand(String fullCommand) {
         String[] toks = fullCommand.split(" ");
-        gScene scene;
         if(toks.length < 2)
             return "null";
-        String tenv = toks[1];
-        boolean isServer = false;
-        if(tenv.equalsIgnoreCase("server")) {
-            scene = cServerLogic.scene;
-            isServer = true;
-        }
-        else if(tenv.equalsIgnoreCase("client"))
-            scene = cClientLogic.scene;
-        else
+        String ttype = toks[1];
+        if(cServerLogic.scene.getThingMap(ttype) == null)
             return "null";
+        HashMap<String, gThing> thingMap = cServerLogic.scene.getThingMap(ttype);
         if(toks.length < 3)
-            return "null";
-        String ttype = toks[2];
-        if(scene.getThingMap(ttype) == null)
-            return "null";
-        HashMap<String, gThing> thingMap = scene.getThingMap(ttype);
-        if(toks.length < 4)
             return thingMap.toString();
-        String tid = toks[3];
+        String tid = toks[2];
         if(!thingMap.containsKey(tid))
             return "null";
         gThing thing = thingMap.get(tid);
-        if(toks.length < 5)
+        if(toks.length < 4)
             return thing.toString();
-        String tk = toks[4];
+        String tk = toks[3];
         if(thing.get(tk) == null)
             return "null";
-        if(toks.length < 6) {
-            if(isServer)
-                nServer.instance().addExcludingNetCmd("server", fullCommand.replaceFirst(tenv, "client"));
+        if(toks.length < 5)
             return thing.get(tk);
-        }
         StringBuilder tvb = new StringBuilder();
-        for(int i = 5; i < toks.length; i++) {
+        for(int i = 4; i < toks.length; i++) {
             tvb.append(" ").append(toks[i]);
         }
         String tv = tvb.substring(1);
@@ -48,8 +32,6 @@ public class xComSetThing extends xCom {
             thing.put(tk, tvr);
         else
             thing.put(tk, tv);
-        if(isServer)
-            nServer.instance().addExcludingNetCmd("server", fullCommand.replaceFirst(tenv, "client"));
         return thing.get(tk);
     }
 }
