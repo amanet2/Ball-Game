121cb8c89d4bfa589509c2853ca26851515acabe
Stallion <tonyjmstallion@gmail.com>
2022-08-09 01:36:10 -0500

    teleporter and stuff:

--- src/gItemTeleporterRed.java
+++ src/gItemTeleporterRed.java
@@ -1,23 +1,22 @@
 public class gItemTeleporterRed extends gItem {
     public void activateItem(gPlayer player) {
         super.activateItem(player);
-        if(player.getInt("stockhp") > 0 && player.isZero("inteleporter")) {
-            gThing exit = null;
-            for(String id : cServerLogic.scene.getThingMap("ITEM_TELEPORTER_RED").keySet()) {
-                gThing teleporter = cServerLogic.scene.getThingMap("ITEM_TELEPORTER_RED").get(id);
-                if(!isVal("id", teleporter.get("id")))
-                    exit = teleporter;
-            }
-            if(exit != null) {
-                player.put("inteleporter", "1");
-                xCon.ex(String.format("exec items/teleporter %s %s %s", player.get("id"),
-                        exit.getInt("coordx") + exit.getInt("dimw")/2 - player.getInt("dimw")/2,
-                        exit.getInt("coordy") + exit.getInt("dimh")/2 - player.getInt("dimh")/2)
-                );
+        if(player.getInt("stockhp") < 1 || player.isOne("inteleporter"))
+            return;
+        gThing exit = null;
+        for(String id : cServerLogic.scene.getThingMap("ITEM_TELEPORTER_RED").keySet()) {
+            gThing teleporter = cServerLogic.scene.getThingMap("ITEM_TELEPORTER_RED").get(id);
+            if(!isVal("id", teleporter.get("id"))) {
+                exit = teleporter;
+                break;
             }
         }
-        else
-            player.put("inteleporter", "1");
+        if(exit == null)
+            return;
+        xCon.ex(String.format("exec items/teleporter %s %s",
+                exit.getInt("coordx") + exit.getInt("dimw")/2 - player.getInt("dimw")/2,
+                exit.getInt("coordy") + exit.getInt("dimh")/2 - player.getInt("dimh")/2)
+        );
     }
     public gItemTeleporterRed(int x, int y) {
         super(x, y, 300, 300);
