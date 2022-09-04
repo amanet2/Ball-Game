eddf1e39b5e2fdc95363ad93bfe2e936c499d731
Stallion <tonyjmstallion@gmail.com>
2022-09-04 04:08:02 -0500

    mapchange script

--- src/xComEditorNewMap.java
+++ src/xComEditorNewMap.java
@@ -2,9 +2,7 @@ import java.util.ArrayList;
 
 public class xComEditorNewMap extends xCom {
     public String doCommand(String fullCommand) {
-        xCon.ex("clearthingmap THING_PLAYER;load;cv_maploaded 1;cv_gamemode 0");
-        nServer.instance().addExcludingNetCmd("server",
-                "cl_clearthingmap THING_PLAYER;cl_load;cv_maploaded 1;cv_gamemode 0");
+        xCon.ex("exec scripts/e_newmap");
         //reset game state
         gScoreboard.resetScoresMap();
         nServer.instance().voteSkipList = new ArrayList<>();
