c6b6dcf720fe1640fbf04f71a9b11f9a33c1f8f2
Stallion <tonyjmstallion@gmail.com>
2022-09-04 03:29:57 -0500

    cut down this code

--- src/xComEditorDelThing.java
+++ src/xComEditorDelThing.java
@@ -5,17 +5,8 @@ public class xComEditorDelThing extends xCom {
                 return "deleted prefab " + cClientLogic.selectedPrefabId;
         }
         if(cClientLogic.selecteditemid.length() > 0) {
-                String toRemoveItemId = "";
-                for(String id : cClientLogic.scene.getThingMap("THING_ITEM").keySet()) {
-                    gThing item = cClientLogic.scene.getThingMap("THING_ITEM").get(id);
-                    if(item.isVal("id", cClientLogic.selecteditemid))
-                        toRemoveItemId = id;
-                }
-                if(toRemoveItemId.length() > 0) {
-                    String cmd = "deleteitem " + toRemoveItemId;
-                    nClient.instance().addNetCmd(cmd);
-                }
-                return "deleted item " + cClientLogic.selecteditemid;
+            nClient.instance().addNetCmd("deleteitem " + cClientLogic.selecteditemid);
+            return "deleted item " + cClientLogic.selecteditemid;
         }
         return "nothing to delete";
     }
