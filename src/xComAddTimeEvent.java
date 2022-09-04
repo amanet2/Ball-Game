fe8966180e94c49720cb06bfdf3c0756236736ac
amanet2 <tonyjmstallion@gmail.com>
2022-09-04 00:59:54 -0500

    example of offloading to this helper method

--- src/xComAddTimeEvent.java
+++ src/xComAddTimeEvent.java
@@ -2,17 +2,9 @@ public class xComAddTimeEvent extends xCom {
     public String doCommand(String fullCommand) {
         if(!sSettings.IS_SERVER)
             return "addevent can only be used by active server";
-        String[] args = fullCommand.split(" ");
-        if(args.length < 3)
+        if(eUtils.argsLength(fullCommand) < 3)
             return "usage: addevent <time> <string to execute>";
-        for(int i = 0; i < args.length; i++) {
-            if(!args[i].startsWith("$"))
-                continue;
-            if(cServerVars.instance().contains(args[i].substring(1)))
-                args[i] = cServerVars.instance().get(args[i].substring(1));
-            else if(sVars.get(args[i]) != null)
-                args[i] = sVars.get(args[i]);
-        }
+        String[] args = eUtils.parseScriptArgsServer(fullCommand);
         StringBuilder act = new StringBuilder("");
         for(int i = 2; i < args.length; i++) {
             act.append(" ").append(args[i]);
