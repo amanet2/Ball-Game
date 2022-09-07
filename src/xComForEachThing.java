7ae16004a33b8b0c392e30b2dbc572dbf5236172
amanet2 <tonyjmstallion@gmail.com>
2022-09-06 23:45:10 -0500

    try a foreach

---
+++ src/xComForEachThing.java
@@ -0,0 +1,6 @@
+public class xComForEachThing extends xCom {
+    //usage: foreach $var $THING_TYPE <script to execute where $var is preloaded>
+    public String doCommand(String fullCommand) {
+        return "usage: foreach $var $THING_TYPE <script to execute where $var is preloaded>";
+    }
+}
