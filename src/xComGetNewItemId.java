60fb0b7ed0176740b4dc0ec7fe3e909053e6567b
Stallion <tonyjmstallion@gmail.com>
2022-12-02 17:18:10 -0600

    move this code

--- src/xComGetNewItemId.java
+++ src/xComGetNewItemId.java
@@ -1,5 +1,10 @@
 public class xComGetNewItemId extends xCom {
     public String doCommand(String fullCommand) {
-        return Integer.toString(cServerLogic.getNewItemId());
+        int itemId = 0;
+        for(String id : cServerLogic.scene.getThingMap("THING_ITEM").keySet()) {
+            if(itemId < Integer.parseInt(id))
+                itemId = Integer.parseInt(id);
+        }
+        return Integer.toString(itemId+1); //want to be the NEXT id
     }
 }
