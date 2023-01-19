faeb91bb41c2d55bff53cb34a99d13b4dfe5a7c3
amanet2 <tonyjmstallion@gmail.com>
2023-01-19 17:56:57 -0600

    re establish standard rates after reading goldsrc forum thread

--- src/sSettings.java
+++ src/sSettings.java
@@ -49,9 +49,9 @@ public class sSettings {
 	static int max_packet_size = 1200;
 	static int rcvbytesclient = 2048;
 	static int rcvbytesserver = 512;
-	static int rateclient = 60;
-	static int rateserver = 1000;
-	static int rategame = 240;
+	static int rateclient = 30;  //client net send rate
+	static int rateserver = 60; //server internal game rate for simulation
+	static int rategame = 240; //client game rate like input, simulation
 	static int ratebots = 30;
 	static boolean smoothing = true;
 }
