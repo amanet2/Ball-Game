779a290e3bcadee90037dc2af0035957740aade5
amanet2 <tonyjmstallion@gmail.com>
2023-02-19 15:57:50 -0600

    another unused comm

--- src/xCon.java
+++ src/xCon.java
@@ -1850,18 +1850,6 @@ public class xCon {
                 return gCamera.get(ck);
             }
         });
-        commands.put("setcammovs", new xCom() {
-            public String doCommand(String fullCommand) {
-                if(eUtils.argsLength(fullCommand) < 5)
-                    return "0";
-                String[] args = eUtils.parseScriptArgsAllSources(fullCommand);
-                gCamera.put("mov0", args[1]);
-                gCamera.put("mov1", args[2]);
-                gCamera.put("mov2", args[3]);
-                gCamera.put("mov3", args[4]);
-                return "1";
-            }
-        });
         commands.put("setnstate", new xCom() {
             //usage: setnstate $id $key $value
             public String doCommand(String fullCommand) {
