ad62100488a920fe033178acb38d4df619bc420f
amanet2 <tonyjmstallion@gmail.com>
2023-02-19 14:44:18 -0600

    start to unify the setvar and config files

--- src/cServerVars.java
+++ src/cServerVars.java
@@ -18,11 +18,14 @@ public class cServerVars extends gArgSet {
         });
         putArg(new gArg("maxhp", "500") {
             public void onChange() {
-                int newmaxhp = Integer.parseInt(value);
-                nServer.instance().addNetCmd("cv_maxhp " + newmaxhp);
-                for(String s : cServerLogic.scene.getThingMap("THING_PLAYER").keySet()) {
-                    gPlayer p = cServerLogic.scene.getPlayerById(s);
-                    p.putInt("stockhp", newmaxhp);
+                xCon.ex("cv_maxhp " + value);
+                if(sSettings.IS_SERVER) {
+                    int newmaxhp = Integer.parseInt(value);
+                    nServer.instance().addNetCmd("cv_maxhp " + newmaxhp);
+                    for (String s : cServerLogic.scene.getThingMap("THING_PLAYER").keySet()) {
+                        gPlayer p = cServerLogic.scene.getPlayerById(s);
+                        p.putInt("stockhp", newmaxhp);
+                    }
                 }
             }
         });
