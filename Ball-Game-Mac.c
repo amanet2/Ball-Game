8f32fe722d400a4bb423f8cc29c3b8e5035046e4
Stallion <tonyjmstallion@gmail.com>
2023-02-24 15:01:48 -0600

    better mac launcher

--- Ball-Game-Mac.c
+++ Ball-Game-Mac.c
@@ -1,9 +1,25 @@
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 int main(int argc, char * argv[]) {
+    char * findstr = "Ball-Game-Mac";
+    char * replacestr = "pkg/run_game.sh ";
+    char * bashstr = "bash ";
+    char * startstr = malloc(strlen(argv[0]) - strlen(findstr) + strlen(replacestr));
+    char * ptr;
+
+    strcpy(startstr, bashstr);
+    strcat(startstr, argv[0]);
+
+    ptr = strstr(startstr, findstr);
+    if(ptr)
+    {
+        memmove(ptr + strlen(replacestr), ptr + strlen(findstr), strlen(ptr + strlen(findstr)) + 1);
+        strncpy(ptr, replacestr, strlen(replacestr));
+    }
+
     int argmalloc = 0;
-    char * startstr = "bash Code/Ball-Game/pkg/run_game.sh ";
     int i;
     for(i = 1; i < argc; i++) {
         argmalloc += sizeof(char)*strlen(argv[i]);
@@ -12,11 +28,13 @@ int main(int argc, char * argv[]) {
     }
     char runstr[sizeof(char)*strlen(startstr) + argmalloc];
     strcpy(runstr, startstr);
+    free(startstr);
     for(i = 1; i < argc; i++) {
         strcat(runstr, argv[i]);
         if(i < argc-1)
             strcat(runstr, " ");
     }
     system(runstr);
+//    free(runstr);
     return 0;
 }
