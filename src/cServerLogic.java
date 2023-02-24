cd5769c7940d84b55f5f1e016f27e25fdd05f8be
amanet2 <tonyjmstallion@gmail.com>
2023-02-23 22:05:44 -0600

    saw this synchronization scheme

--- src/cServerLogic.java
+++ src/cServerLogic.java
@@ -5,7 +5,7 @@ public class cServerLogic {
     static long timeleft = 120000;
     static int listenPort = 5555;
     static gScene scene;
-    static gTimeEventSet timedEvents = new gTimeEventSet();
+    static final gTimeEventSet timedEvents = new gTimeEventSet();
 
     static void changeMap(String mapPath) {
         xCon.ex(String.format("exec scripts/changemap %s", mapPath));
@@ -17,14 +17,16 @@ public class cServerLogic {
         if(sSettings.show_mapmaker_ui)
             return;
         long starttime = gTime.gameTime;
-        for(long t = starttime+1000; t <= starttime+timelimit; t+=1000) {
-            long lastT = t;
-            timedEvents.put(Long.toString(t), new gTimeEvent() {
-                public void doCommand() {
-                    if(timelimit > 0)
-                        timeleft = Math.max(0, (starttime + timelimit) - lastT);
-                }
-            });
+        synchronized (timedEvents) {
+            for (long t = starttime + 1000; t <= starttime + timelimit; t += 1000) {
+                long lastT = t;
+                timedEvents.put(Long.toString(t), new gTimeEvent() {
+                    public void doCommand() {
+                        if (timelimit > 0)
+                            timeleft = Math.max(0, (starttime + timelimit) - lastT);
+                    }
+                });
+            }
         }
         xCon.ex("exec scripts/startgame " + cClientLogic.gamemode);
     }
