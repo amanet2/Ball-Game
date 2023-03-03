461eb21422577e66799b8f2fb9460674e1fa9a60
amanet2 <tonyjmstallion@gmail.com>
2023-03-03 14:56:40 -0600

    getaddint done

--- src/xCon.java
+++ src/xCon.java
@@ -949,32 +949,31 @@ public class xCon {
                 return ss;
             }
         });
-        commands.put("getaddint", new xCom() {
+        commands.put("addint", new xCom() {
             public String doCommand(String fullCommand) {
-                //usage: getaddint $result $num1 $num2
-                if(eUtils.argsLength(fullCommand) < 4)
+                //usage: addint $num1 $num2 //return result (use getres)
+                if(eUtils.argsLength(fullCommand) < 3)
                     return "null";
                 String[] args = eUtils.parseScriptArgsServer(fullCommand);
-                String tk = args[1];
-                Number n1 = null;
-                Number n2 = null;
+                Number n1;
+                Number n2;
                 try {
-                    n1 = NumberFormat.getInstance().parse(args[2]);
-                    n2 = NumberFormat.getInstance().parse(args[3]);
+                    n1 = NumberFormat.getInstance().parse(args[1]);
+                    n2 = NumberFormat.getInstance().parse(args[2]);
                 } catch (ParseException e) {
                     e.printStackTrace();
+                    return "null";
                 }
                 boolean n1d = n1 instanceof Double;
                 boolean n1l = n1 instanceof Long;
                 boolean n2d = n2 instanceof Double;
                 boolean n2l = n2 instanceof Long;
                 if(n1d || n2d)
-                    cServerVars.instance().put(tk, Integer.toString((int) ((double) n1 + (double) n2)));
+                    return Integer.toString((int) ((double) n1 + (double) n2));
                 else if(n1l || n2l)
-                    cServerVars.instance().put(tk, Long.toString((long) n1 + (long) n2));
+                    return Long.toString((long) n1 + (long) n2);
                 else
-                    cServerVars.instance().put(tk, Integer.toString((int) n1 + (int) n2));
-                return cServerVars.instance().get(tk);
+                    return Integer.toString((int) n1 + (int) n2);
             }
         });
         commands.put("getaddlong", new xCom() {
