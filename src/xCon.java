9b55760f49375c076851a5a28c85ecc9839a038c
amanet2 <tonyjmstallion@gmail.com>
2023-03-04 02:20:45 -0600

    combine putitem stuff'

--- src/xCon.java
+++ src/xCon.java
@@ -1579,21 +1579,7 @@ public class xCon {
                 String[] toks = fullCommand.split(" ");
                 if(toks.length < 5)
                     return "usage: putitem <ITEM_TITLE> <id> <x> <y>";
-                String itemTitle = toks[1];
-                String itemId = toks[2];
-                int iw = Integer.parseInt(ex("setvar " + itemTitle+"_dimw"));
-                int ih = Integer.parseInt(ex("setvar " + itemTitle+"_dimh"));
-                String isp = ex("setvar " + itemTitle + "_sprite");
-                String isc = ex("setvar " + itemTitle + "_script");
-                gItem item = new gItem(itemTitle, Integer.parseInt(toks[3]), Integer.parseInt(toks[4]), iw, ih,
-                        isp.equalsIgnoreCase("null") ? null : gTextures.getGScaledImage(eUtils.getPath(isp),
-                                iw, ih));
-                if(!isc.equalsIgnoreCase("null"))
-                    item.put("script", isc);
-                item.put("id", itemId);
-                item.put("occupied", "0");
-                cServerLogic.scene.getThingMap("THING_ITEM").put(itemId, item);
-                cServerLogic.scene.getThingMap(item.get("type")).put(itemId, item);
+                putItemDelegate(toks, cServerLogic.scene);
                 return "put item";
             }
         });
@@ -1602,20 +1588,7 @@ public class xCon {
                 String[] toks = fullCommand.split(" ");
                 if(toks.length < 5)
                     return "usage: cl_putitem <ITEM_TITLE> <id> <x> <y>";
-                String itemTitle = toks[1];
-                String itemId = toks[2];
-                int iw = Integer.parseInt(ex("setvar " + itemTitle+"_dimw"));
-                int ih = Integer.parseInt(ex("setvar " + itemTitle+"_dimh"));
-                String isp = ex("setvar " + itemTitle + "_sprite");
-                String isc = ex("setvar " + itemTitle + "_script");
-                gItem item = new gItem(itemTitle, Integer.parseInt(toks[3]), Integer.parseInt(toks[4]), iw, ih,
-                        isp.equalsIgnoreCase("null") ? null : gTextures.getGScaledImage(eUtils.getPath(isp),
-                                iw, ih));
-                if(!isc.equalsIgnoreCase("null"))
-                    item.put("script", isc);
-                item.put("id", itemId);
-                cClientLogic.scene.getThingMap("THING_ITEM").put(itemId, item);
-                cClientLogic.scene.getThingMap(item.get("type")).put(itemId, item);
+                putItemDelegate(toks, cClientLogic.scene);
                 return "cl_put item";
             }
         });
@@ -2198,6 +2171,24 @@ public class xCon {
         if(instance == null)
             instance = new xCon();
         return instance;
+    }
+
+    private void putItemDelegate(String[] toks, gScene scene) {
+        String itemTitle = toks[1];
+        String itemId = toks[2];
+        int iw = Integer.parseInt(ex("setvar " + itemTitle+"_dimw"));
+        int ih = Integer.parseInt(ex("setvar " + itemTitle+"_dimh"));
+        String isp = ex("setvar " + itemTitle + "_sprite");
+        String isc = ex("setvar " + itemTitle + "_script");
+        gItem item = new gItem(itemTitle, Integer.parseInt(toks[3]), Integer.parseInt(toks[4]), iw, ih,
+                isp.equalsIgnoreCase("null") ? null : gTextures.getGScaledImage(eUtils.getPath(isp),
+                        iw, ih));
+        if(!isc.equalsIgnoreCase("null"))
+            item.put("script", isc);
+        item.put("id", itemId);
+        item.put("occupied", "0");
+        scene.getThingMap("THING_ITEM").put(itemId, item);
+        scene.getThingMap(item.get("type")).put(itemId, item);
     }
 
     public static String ex(String s) {
