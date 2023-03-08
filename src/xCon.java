f2488660ec52c1893f5e076cb1943142cd79e625
amanet2 <tonyjmstallion@gmail.com>
2023-03-08 11:33:17 -0600

    lunch

--- src/xCon.java
+++ src/xCon.java
@@ -269,38 +269,16 @@ public class xCon {
         commands.put("clearthingmap", new xCom() {
             public String doCommand(String fullCommand) {
                 String[] toks = fullCommand.split(" ");
-                if (toks.length > 1) {
-                    String thing_title = toks[1];
-                    ArrayList<String> toRemoveIds = new ArrayList<>();
-                    if(thing_title.contains("ITEM_")) {
-                        if(cServerLogic.scene.objectMaps.containsKey(thing_title))
-                            toRemoveIds.addAll(cServerLogic.scene.getThingMap(thing_title).keySet());
-                        for(String id : toRemoveIds) {
-                            cServerLogic.scene.getThingMap("THING_ITEM").remove(id);
-                        }
-                    }
-                    if(cServerLogic.scene.objectMaps.containsKey(thing_title))
-                        cServerLogic.scene.objectMaps.put(thing_title, new LinkedHashMap<>());
-                }
+                if (toks.length > 1)
+                    clearThingMapDelegate(toks, cServerLogic.scene);
                 return "usage: clearthingmap <thing_title>";
             }
         });
         commands.put("cl_clearthingmap", new xCom() {
             public String doCommand(String fullCommand) {
                 String[] toks = fullCommand.split(" ");
-                if (toks.length > 1) {
-                    String thing_title = toks[1];
-                    ArrayList<String> toRemoveIds = new ArrayList<>();
-                    if(thing_title.contains("ITEM_")) {
-                        if(cClientLogic.scene.objectMaps.containsKey(thing_title))
-                            toRemoveIds.addAll(cClientLogic.scene.getThingMap(thing_title).keySet());
-                        for(String id : toRemoveIds) {
-                            cClientLogic.scene.getThingMap("THING_ITEM").remove(id);
-                        }
-                    }
-                    if(cClientLogic.scene.objectMaps.containsKey(thing_title))
-                        cClientLogic.scene.objectMaps.put(thing_title, new LinkedHashMap<>());
-                }
+                if (toks.length > 1)
+                    clearThingMapDelegate(toks, cClientLogic.scene);
                 return "usage: clearthingmap <thing_title>";
             }
         });
@@ -1879,6 +1857,20 @@ public class xCon {
         String tv = tvb.substring(1);
         thing.put(tk, tv);
         return thing.get(tk);
+    }
+
+    private void clearThingMapDelegate(String[] toks, gScene scene) {
+        String thing_title = toks[1];
+        ArrayList<String> toRemoveIds = new ArrayList<>();
+        if(thing_title.contains("ITEM_")) {
+            if(scene.objectMaps.containsKey(thing_title))
+                toRemoveIds.addAll(scene.getThingMap(thing_title).keySet());
+            for(String id : toRemoveIds) {
+                scene.getThingMap("THING_ITEM").remove(id);
+            }
+        }
+        if(scene.objectMaps.containsKey(thing_title))
+            scene.objectMaps.put(thing_title, new LinkedHashMap<>());
     }
 
     private String testResDelegate(String[] args) {
