9df96940f74fa6da0e02bac52e65bcd75200dd60
Stallion <tonyjmstallion@gmail.com>
2023-04-01 04:52:36 -0500

    dont need these anymore

--- src/xCon.java
+++ src/xCon.java
@@ -1366,22 +1366,6 @@ public class xCon {
                 return fullCommand;
             }
         });
-        commands.put("setcam", new xCom() {
-            //usage: setcam $key $val
-            public String doCommand(String fullCommand) {
-                if(eUtils.argsLength(fullCommand) < 2)
-                    return "null";
-                String[] args = eUtils.parseScriptArgsAllSources(fullCommand);
-                String ck = args[1];
-                String sv = "null";
-                if(gCamera.contains(ck))
-                    sv = gCamera.get(ck);
-                if(args.length < 3)
-                    return sv;
-                gCamera.put(ck, args[2]);
-                return gCamera.get(ck);
-            }
-        });
         commands.put("setnstate", new xCom() {
             //usage: setnstate $id $key $value
             public String doCommand(String fullCommand) {
