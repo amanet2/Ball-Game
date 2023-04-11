a3f6f3e2ad85e548197f284c11045d356a3c9749
Stallion <tonyjmstallion@gmail.com>
2023-04-11 02:48:25 -0500

    decent one'

--- src/xCon.java
+++ src/xCon.java
@@ -941,27 +941,6 @@ public class xCon {
                 return cServerVars.instance().get(tk);
             }
         });
-        commands.put("cl_getres", new xCom() {
-            public String doCommand(String fullCommand) {
-                if(eUtils.argsLength(fullCommand) < 2)
-                    return "null";
-                String[] args = eUtils.parseScriptArgsClient(fullCommand);
-                String tk = args[1];
-                if(args.length < 3) {
-                    if (!cClientVars.instance().contains(tk))
-                        return "null";
-                    return cClientVars.instance().get(tk);
-                }
-                StringBuilder tvb = new StringBuilder();
-                for(int i = 2; i < args.length; i++) {
-                    tvb.append(" ").append(args[i]);
-                }
-                String tv = tvb.substring(1);
-                String res = ex(tv);
-                cClientVars.instance().put(tk, res);
-                return cClientVars.instance().get(tk);
-            }
-        });
         commands.put("getsnap", new xCom() {
             //usage: getsnap $id $key
             public String doCommand(String fullCommand) {
