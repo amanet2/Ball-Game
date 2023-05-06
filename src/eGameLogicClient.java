c133874493ea6528181818cee94cecc1ea9082db
amanet2 <tonyjmstallion@gmail.com>
2023-05-05 20:28:05 -0500

    use state and map for server reading stuff

--- src/eGameLogicClient.java
+++ src/eGameLogicClient.java
@@ -73,27 +73,27 @@ public class eGameLogicClient extends eGameLogicAdapter {
     public void readData(String receiveDataString) {
         StringBuilder foundIdsBuilder = new StringBuilder();
         String netmapstring = receiveDataString.trim();
-        HashMap<String, HashMap<String, String>> packargmap = getMapFromNetMapString(netmapstring);
-        for(String idload : packargmap.keySet()) {
-            HashMap<String, String> packArgs = new HashMap<>(packargmap.get(idload));
+        nStateMap packArgStateMap = new nStateMap(netmapstring);
+        for(String idload : packArgStateMap.keys()) {
+            nState packArgState = packArgStateMap.get(idload);
             // SERVER time and cmd
             if(idload.equals("server")) {
-                for (String k : packArgs.keySet()) {
-                    receivedArgsServer.put(k, packArgs.get(k));
+                for (String k : packArgState.keys()) {
+                    receivedArgsServer.put(k, packArgState.get(k));
                 }
             }
             else {
                 if(!clientStateMap.contains(idload))
                     clientStateMap.put(idload, new nStateBallGameClient());
-                for(String k : packArgs.keySet()) {
-                    clientStateMap.get(idload).put(k, packArgs.get(k));
+                for(String k : packArgState.keys()) {
+                    clientStateMap.get(idload).put(k, packArgState.get(k));
                 }
                 foundIdsBuilder.append(", ").append(idload);
             }
             if(idload.equals(uiInterface.uuid)) { // handle our own player to get things like stockhp from server
                 gPlayer userPlayer = cClientLogic.getUserPlayer();
-                if(userPlayer != null && packArgs.containsKey("hp"))
-                    userPlayer.put("stockhp", packArgs.get("hp"));
+                if(userPlayer != null && packArgState.contains("hp"))
+                    userPlayer.put("stockhp", packArgState.get("hp"));
             }
         }
         String foundIds = foundIdsBuilder.substring(1);
@@ -109,26 +109,6 @@ public class eGameLogicClient extends eGameLogicAdapter {
             }
         }
         clientStateSnapshot = clientStateMap.toString().replace(", ", ",");
-    }
-
-    public HashMap<String, HashMap<String, String>> getMapFromNetMapString(String argload) {
-        HashMap<String, HashMap<String, String>> toReturn = new HashMap<>();
-        String modstr = argload.substring(1,argload.length()-1);
-        String[] idsplit = modstr.split("},");
-        for(String s : idsplit) {
-            String idtok = s.substring(0,s.indexOf("={"));
-            toReturn.put(idtok, new HashMap<>());
-            String argstr = s.replace(idtok+"={","");
-            if(argstr.length() < 1)
-                continue;
-            if(argstr.charAt(argstr.length()-1) == '}')
-                argstr = argstr.substring(0, argstr.length()-1);
-            for(String pair : argstr.split(",")) {
-                String[] vals = pair.split("=");
-                toReturn.get(idtok).put(vals[0].trim(), vals.length > 1 ? vals[1].trim() : "");
-            }
-        }
-        return toReturn;
     }
 
     @Override
