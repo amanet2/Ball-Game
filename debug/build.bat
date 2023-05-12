2b82e85aad36e8a9e13e03ffdf8e3b7190917d00
amanet2 <tonyjmstallion@gmail.com>
2023-05-12 03:49:59 -0500

    try to whittle down what's needed here

--- debug/build.bat
+++ debug/build.bat
@@ -1,7 +1,7 @@
 set PATH=%~dp0..\bin\jdk-18.0.2.1\bin\;%~dp0..\bin\javafx-sdk-11.0.2\bin\
-javac --module-path %~dp0..\lib --add-modules=javafx.media -d %~dp0..\pkg %~dp0..\src\*.java
+javac --module-path %~dp0..\bin\javafx-sdk-11.0.2\lib --add-modules=javafx.media -d %~dp0..\pkg %~dp0..\src\*.java
 cd %~dp0..\pkg
-jar cmf MANIFEST.MF BALL_GAME.jar *.class ..\src\*.java ballgame
+jar cmf MANIFEST.MF BALL_GAME.jar *.class
 del *.class
 cd %~dp0
 REM pause
--- pkg/debug_game.bat
+++ pkg/debug_game.bat
@@ -1,5 +1,5 @@
 @echo off
 cd %~dp0
 set PATH=%~dp0..\bin\jdk-18.0.2.1\bin\;%~dp0..\bin\javafx-sdk-11.0.2\bin\
-java -Dsun.java2d.uiScale=1.0 -jar --module-path ..\lib --add-modules=javafx.media BALL_GAME.jar %*
+java -Dsun.java2d.uiScale=1.0 -jar --module-path %~dp0..\bin\javafx-sdk-11.0.2\lib --add-modules=javafx.media BALL_GAME.jar %*
 REM exit
