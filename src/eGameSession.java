a6e946d835ee5d52e160f7bd6668f23c8de125d6
amanet2 <tonyjmstallion@gmail.com>
2023-05-11 22:34:15 -0500

    save a few lines on timeing and session

--- src/eGameSession.java
+++ src/eGameSession.java
@@ -1,13 +1,10 @@
 public class eGameSession extends Thread implements Runnable {
-    private final eTimer timer;
     private final eGameLogic gameLogic;
-    private long tickTimeNanos;
     private final int tickRate;
     private boolean playing;
 
     public eGameSession(eGameLogic logic, int rate) {
         gameLogic = logic;
-        timer = new eTimer();
         tickRate = rate;
         playing = true;
         ((eGameLogicAdapter) logic).setParentSession(this);
@@ -17,73 +14,38 @@ public class eGameSession extends Thread implements Runnable {
     @Override
     public void run() {
         try {
-            init();
-            loop();
+            long snapshotTimeNanos = System.nanoTime();
+            long tickTimeNanos = snapshotTimeNanos;
+            gameLogic.init();
+            while (playing) {
+                snapshotTimeNanos = System.nanoTime();
+                gameLogic.input();
+                while(tickTimeNanos < snapshotTimeNanos) {
+                    tickTimeNanos += (1000000000/tickRate);
+                    gameLogic.update();
+                }
+                gameLogic.render();
+                if(tickRate > 0) {
+                    long nextFrameTime = (snapshotTimeNanos + (1000000000/tickRate));
+                    while (nextFrameTime > System.nanoTime()) {
+                        try {
+                            Thread.sleep(1);
+                        }
+                        catch (InterruptedException ignored) {
+                        }
+                    }
+                }
+            }
+            System.out.println("SESSION LOOP EXITED " + gameLogic.toString());
         }
         catch (Exception err) {
             err.printStackTrace();
         }
         finally {
-            cleanup();
-        }
+            gameLogic.cleanup();        }
     }
 
     public void destroy() {
         playing = false;
-    }
-
-    private void init(){
-        timer.init();
-        tickTimeNanos = timer.snapshotTimeNanos();
-        gameLogic.init();
-    }
-
-    private void cleanup() {
-        gameLogic.cleanup();
-    }
-
-    private void loop() {
-        while (playing) {
-            timer.sync();
-            input();
-
-            while(behindTimer()) {
-                update();
-            }
-            render();
-            sync();
-        }
-        System.out.println("SESSION LOOP EXITED " + gameLogic.toString());
-    }
-
-    private boolean behindTimer() {
-        return tickTimeNanos < timer.snapshotTimeNanos();
-    }
-
-    private void input() {
-        gameLogic.input();
-    }
-
-    private void update() {
-        tickTimeNanos += (1000000000/tickRate);
-        gameLogic.update();
-    }
-
-    private void render() {
-        gameLogic.render();
-    }
-
-    private void sync() {
-        // framerate limit
-        if(tickRate > 0) {
-            long nextFrameTime = (timer.snapshotTimeNanos() + (1000000000/tickRate));
-            while (nextFrameTime > timer.currentTimeNanos()) {
-                try {
-                    Thread.sleep(1);
-                }
-                catch (InterruptedException ignored) {
-                }
-            }
-        }
     }
 }
