745217732b1722f7dac244b7e04f01ed380ef0e3
Stallion <tonyjmstallion@gmail.com>
2023-05-12 10:25:55 -0500

    looks like only the last .class arg on line 3 in build is the only need for cd

--- debug/build.sh
+++ debug/build.sh
@@ -1,6 +1,5 @@
-#export PATH="$(cd $(dirname "$1");pwd)"/../bin/jdk-18.jdk/Contents/Home/bin/:${PATH}
 "$(cd $(dirname "$1");pwd)"/../bin/jdk-18.jdk/Contents/Home/bin/javac --module-path "$(cd $(dirname "$1");pwd)"/../bin/javafx-sdk-18/lib --add-modules=javafx.media -d "$(cd $(dirname "$1");pwd)"/../pkg "$(cd $(dirname "$1");pwd)"/../src/*.java
 cd "$(cd $(dirname "$1");pwd)"/../pkg
-"$(cd $(dirname "$1");pwd)"/../bin/jdk-18.jdk/Contents/Home/bin/jar cmf MANIFEST.MF BALL_GAME.jar *.class
-rm *.class
+"$(cd $(dirname "$1");pwd)"/../bin/jdk-18.jdk/Contents/Home/bin/jar cmf "$(cd $(dirname "$1");pwd)"/../pkg/MANIFEST.MF "$(cd $(dirname "$1");pwd)"/../pkg/BALL_GAME.jar *.class
+rm "$(cd $(dirname "$1");pwd)"/../pkg/*.class
 cd "$(cd $(dirname "$1");pwd)"
