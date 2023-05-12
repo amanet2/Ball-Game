be060bff0df5602072f3f83b1887e6367965ee1d
Stallion <tonyjmstallion@gmail.com>
2023-05-12 04:42:13 -0500

    mac scripts

--- pkg/run_game.sh
+++ pkg/run_game.sh
@@ -1,4 +1,4 @@
 cd "$(cd $(dirname "$0");pwd)"
 export PATH="$(cd $(dirname "$0");pwd)"/../bin/jdk-18.jdk/Contents/Home/bin/:"$(cd $(dirname "$0");pwd)"/../bin/javafx-sdk-18/lib/:${PATH}
-java -Dsun.java2d.uiScale=1.0 -jar --module-path ../lib --add-modules=javafx.media BALL_GAME.jar "$@"
+java -Dsun.java2d.uiScale=1.0 -jar --module-path "$(cd $(dirname "$0");pwd)"/../bin/javafx-sdk-18/lib --add-modules=javafx.media BALL_GAME.jar "$@"
 exit
