6d678616781f2e23bd6aceb4a050d052694e54b6
Stallion <tonyjmstallion@gmail.com>
2023-05-12 04:48:31 -0500

    one line run

--- pkg/run_game.sh
+++ pkg/run_game.sh
@@ -1,4 +1,1 @@
-cd "$(cd $(dirname "$0");pwd)"
-export PATH="$(cd $(dirname "$0");pwd)"/../bin/jdk-18.jdk/Contents/Home/bin/:${PATH}
-java -Dsun.java2d.uiScale=1.0 -jar --module-path "$(cd $(dirname "$0");pwd)"/../bin/javafx-sdk-18/lib --add-modules=javafx.media BALL_GAME.jar "$@"
-exit
+"$(cd $(dirname "$0");pwd)"/../bin/jdk-18.jdk/Contents/Home/bin/java -Dsun.java2d.uiScale=1.0 -jar --module-path "$(cd $(dirname "$0");pwd)"/../bin/javafx-sdk-18/lib --add-modules=javafx.media BALL_GAME.jar "$@"
