551bcde36f440260460701f542407aecec3c6dc5
Stallion <tonyjmstallion@gmail.com>
2023-05-12 10:14:33 -0500

    get build script working without path

--- debug/build.sh
+++ debug/build.sh
@@ -1,6 +1,6 @@
-export PATH="$(cd $(dirname "$1");pwd)"/../bin/jdk-18.jdk/Contents/Home/bin/:${PATH}
-javac --module-path "$(cd $(dirname "$1");pwd)"/../bin/javafx-sdk-18/lib --add-modules=javafx.media -d "$(cd $(dirname "$1");pwd)"/../pkg "$(cd $(dirname "$1");pwd)"/../src/*.java
+#export PATH="$(cd $(dirname "$1");pwd)"/../bin/jdk-18.jdk/Contents/Home/bin/:${PATH}
+"$(cd $(dirname "$1");pwd)"/../bin/jdk-18.jdk/Contents/Home/bin/javac --module-path "$(cd $(dirname "$1");pwd)"/../bin/javafx-sdk-18/lib --add-modules=javafx.media -d "$(cd $(dirname "$1");pwd)"/../pkg "$(cd $(dirname "$1");pwd)"/../src/*.java
 cd "$(cd $(dirname "$1");pwd)"/../pkg
-jar cmf MANIFEST.MF BALL_GAME.jar *.class
+"$(cd $(dirname "$1");pwd)"/../bin/jdk-18.jdk/Contents/Home/bin/jar cmf MANIFEST.MF BALL_GAME.jar *.class
 rm *.class
 cd "$(cd $(dirname "$1");pwd)"
