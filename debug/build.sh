2c467cde4bdfe863817e3008975e682b0fa9c03e
Stallion <tonyjmstallion@gmail.com>
2023-05-12 04:44:03 -0500

    path stuff

--- debug/build.sh
+++ debug/build.sh
@@ -1,4 +1,4 @@
-export PATH="$(cd $(dirname "$1");pwd)"/../bin/jdk-18.jdk/Contents/Home/bin/:"$(cd $(dirname "$1");pwd)"/../bin/javafx-sdk-18/lib/:${PATH}
+export PATH="$(cd $(dirname "$1");pwd)"/../bin/jdk-18.jdk/Contents/Home/bin/:${PATH}
 javac --module-path "$(cd $(dirname "$1");pwd)"/../bin/javafx-sdk-18/lib --add-modules=javafx.media -d "$(cd $(dirname "$1");pwd)"/../pkg "$(cd $(dirname "$1");pwd)"/../src/*.java
 cd "$(cd $(dirname "$1");pwd)"/../pkg
 jar cmf MANIFEST.MF BALL_GAME.jar *.class
