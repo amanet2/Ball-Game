f24ea167353f8fd3d6c40883413a2601f96d2d96
amanet2 <tonyjmstallion@gmail.com>
2023-05-12 20:01:41 -0500

    hide terminal

--- pkg/run_game.bat
+++ pkg/run_game.bat
@@ -1,4 +1,4 @@
 @echo off
 cd %~dp0
-%~dp0..\bin\jdk-18.0.2.1\bin\javaw -Dsun.java2d.uiScale=1.0 -jar --module-path %~dp0..\bin\javafx-sdk-11.0.2\lib --add-modules=javafx.media BALL_GAME.jar %*
+start %~dp0..\bin\jdk-18.0.2.1\bin\javaw -Dsun.java2d.uiScale=1.0 -jar --module-path %~dp0..\bin\javafx-sdk-11.0.2\lib --add-modules=javafx.media BALL_GAME.jar %*
 exit
