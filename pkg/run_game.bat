40ca5d95e173708e54b3331f4dbbe09bccf623c4
amanet2 <tonyjmstallion@gmail.com>
2023-05-12 21:29:41 -0500

    works in explorer but not in intellij

--- pkg/run_game.bat
+++ pkg/run_game.bat
@@ -1,4 +1,2 @@
 @echo off
-cd %~dp0
-start %~dp0..\bin\jdk-20.0.1\bin\javaw -Dsun.java2d.uiScale=1.0 -jar --module-path %~dp0..\bin\javafx-sdk-11.0.2\lib --add-modules=javafx.media BALL_GAME.jar %*
-exit
+start %~dp0..\bin\jdk-20.0.1\bin\javaw -Dsun.java2d.uiScale=1.0 -jar --module-path %~dp0..\bin\javafx-sdk-11.0.2\lib --add-modules=javafx.media %~dp0BALL_GAME.jar %*
