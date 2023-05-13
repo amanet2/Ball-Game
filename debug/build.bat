802dc9d7bac326de71621254b6c432602c774fcb
amanet2 <tonyjmstallion@gmail.com>
2023-05-12 20:15:28 -0500

    new jdk

--- debug/build.bat
+++ debug/build.bat
@@ -1,5 +1,5 @@
-%~dp0..\bin\jdk-18.0.2.1\bin\javac --module-path %~dp0..\bin\javafx-sdk-11.0.2\lib --add-modules=javafx.media -d %~dp0..\pkg %~dp0..\src\*.java
+%~dp0..\bin\jdk-20.0.1\bin\javac --module-path %~dp0..\bin\javafx-sdk-11.0.2\lib --add-modules=javafx.media -d %~dp0..\pkg %~dp0..\src\*.java
 cd %~dp0..\pkg
-%~dp0..\bin\jdk-18.0.2.1\bin\jar cmf %~dp0..\debug\MANIFEST.MF %~dp0..\pkg\BALL_GAME.jar *.class
+%~dp0..\bin\jdk-20.0.1\bin\jar cmf %~dp0..\debug\MANIFEST.MF %~dp0..\pkg\BALL_GAME.jar *.class
 del %~dp0..\pkg\*.class
 cd %~dp0
