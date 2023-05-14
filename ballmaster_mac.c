9f18a500b39580d8ec70aaeceb091bd4ec4d5c4a
Stallion <tonyjmstallion@gmail.com>
2023-05-13 22:32:16 -0500

    work on mac executable script and update debug script for mac

--- ballmaster_mac.c
+++ ballmaster_mac.c
@@ -1,14 +1,15 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 int main(int argc, char * argv[]) {
     char * findstr = "/ballmaster_mac";
-    char * replacestr = "/run_game.sh "; //edit here for mapmaker
-    char * bashstr = "bash ";
+    char * replacestr = "/bin/jdk-18.jdk/Contents/Home/bin/java -cp pkg -Dsun.java2d.uiScale=1.0 -jar BALL_GAME.jar "; //edit here for mapmaker
     char * startstr = malloc(strlen(argv[0]) - strlen(findstr) + strlen(replacestr));
     char * ptr;
 
-    strcpy(startstr, bashstr);
+    strcpy(startstr, "");
     strcat(startstr, argv[0]);
 
     ptr = strstr(startstr, findstr);
@@ -33,7 +34,8 @@ int main(int argc, char * argv[]) {
         if(i < argc-1)
             strcat(runstr, " ");
     }
-    system(runstr);
+//    printf("%s", runstr);
+    system("cd ~/Code/Ball-Game/pkg && ~/Code/Ball-Game/bin/jdk-18.jdk/Contents/Home/bin/java -Dsun.java2d.uiScale=1.0 -jar ~/Code/Ball-Game/pkg/BALL_GAME.jar");
 //    free(runstr);
     return 0;
 }
