d6c9196cabbfa5dd5ede6cecb2c563a0ad3e0ee7
amanet2 <tonyjmstallion@gmail.com>
2023-05-18 00:31:56 -0500

    simpl

--- src/dThings.java
+++ src/dThings.java
@@ -2,19 +2,23 @@ import java.awt.*;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Rectangle2D;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class dThings {
     public static void drawBlockFloors(Graphics2D g2) {
         g2.setPaint(xMain.shellLogic.floorTexture);
-        while(xMain.shellLogic.drawFloorsQueue.size() > 0) {
-            int[] dvars = xMain.shellLogic.drawFloorsQueue.remove();
+        Queue<int[]> drawQueueCopy = new LinkedList<>(xMain.shellLogic.drawFloorsQueue);
+        while(drawQueueCopy.size() > 0) {
+            int[] dvars = drawQueueCopy.remove();
             g2.fillRect(dvars[0], dvars[1], dvars[2], dvars[3]);
         }
     }
 
     public static void drawBlockWallsAndPlayersNew(Graphics2D g2) {
-        while(xMain.shellLogic.drawWallsAndPlayersQueue.size() > 0) {
-            dDrawPayload drawPayload = xMain.shellLogic.drawWallsAndPlayersQueue.remove();
+        Queue<dDrawPayload> drawQueueCopy = new LinkedList<>(xMain.shellLogic.drawWallsAndPlayersQueue);
+        while(drawQueueCopy.size() > 0) {
+            dDrawPayload drawPayload = drawQueueCopy.remove();
             if(drawPayload.sprites.length > 0) {
                 if(drawPayload.sprites[0] != null) { //player or object
                     //shadows
@@ -303,41 +307,5 @@ public class dThings {
                 player.getInt("coordy") + player.getInt("dimh")/2 - diff,
                 null);
         g2.setTransform(backup);
-    }
-
-    public static void drawItem(Graphics2D g2, gItem item) {
-        if(item.sprite != null) {
-            //item shadow
-//            drawThingShadow(g2, item);
-            g2.drawImage(item.sprite,
-                    item.getInt("coordx"),
-                    item.getInt("coordy"),
-                    null
-            );
-            if(sSettings.vfxenableflares && !item.get("flare").equals("null")) {
-                String[] flareToks = item.get("flare").split(":");
-                int[] flareArgs = new int[] {
-                        Integer.parseInt(flareToks[0]),
-                        Integer.parseInt(flareToks[1]),
-                        Integer.parseInt(flareToks[2]),
-                        Integer.parseInt(flareToks[3])
-                };
-                dScreenFX.drawFlareFromColor(g2,
-                        item.getInt("coordx") - item.getInt("dimw")/2,
-                        item.getInt("coordy") - item.getInt("dimh")/2,
-                        item.getInt("dimw")*2,
-                        item.getInt("dimh")*2,
-                        1, new Color(flareArgs[0], flareArgs[1], flareArgs[2], flareArgs[3]), new Color(0,0,0,0)
-                );
-            }
-        }
-        else if(sSettings.show_mapmaker_ui){
-            dFonts.setFontColor(g2, "clrf_spawnpoint");
-            g2.fillRect(
-                    item.getInt("coordx"),
-                    item.getInt("coordy"),
-                    item.getInt("dimw"),
-                    item.getInt("dimh"));
-        }
     }
 }
