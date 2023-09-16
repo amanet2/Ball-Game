02aaf9be79d4f17b6e1502e35c02c54a2b1fd9c6
amanet2 <tonyjmstallion@gmail.com>
2023-09-16 03:32:08 -0500

    bad commit, ruminating on the health recharging script

--- python/custom/sv_checkgamestate.py
+++ python/custom/sv_checkgamestate.py
@@ -1,3 +1,23 @@
+import time
+
+
 def check_game_state(game_state):
     for client in game_state.keys():
         game_state[client]["mov1"] = "1"
+        # WIP: Check health recharge
+        # TODO: hprechargetime is set when damage occurs, so this prolly should be in java anyways
+        # TODO: OR everytime we check, we save the old hp and if the new check is less than old hp, we know we took dmg
+        gametime = time.time()  # gametime should be sent to python from java
+        delayhp = 3600
+        rctime = game_state[client]["hprechargetime"]
+        if rctime == "null":
+            game_state[client]["hprechargetime"] = "null"  # hprechargetime, playervars needs to be loaded to python
+            rctime = 0
+        rcplusdelay = rctime + delayhp
+        healdue = gametime >= rcplusdelay
+        if healdue:
+            playerhp = game_state[client]["hp"]
+            if playerhp != "null":
+                newhp = playerhp + 1
+                if newhp <= 500:  # maxhp, config vars need to be loaded into python
+                    game_state[client]["hp"] = newhp
