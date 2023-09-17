8c70ce3393189d109cccb8813a9d29b77f9e04e7
amanet2 <tonyjmstallion@gmail.com>
2023-09-17 00:57:11 -0500

    bad commit, hone python

--- python/custom/sv_checkgamestate.py
+++ python/custom/sv_checkgamestate.py
@@ -3,6 +3,7 @@ import time
 player_recharge_times = {}
 player_old_hps = {}
 
+
 def check_game_state(game_state):
     for client in game_state.keys():
         game_state[client]["mov1"] = "1"
@@ -11,15 +12,18 @@ def check_game_state(game_state):
         # TODO: OR everytime we check, we save the old hp and if the new check is less than old hp, we know we took dmg
         gametime = time.time()  # gametime should be sent to python from java
         delayhp = 3600
-        rctime = game_state[client]["hprechargetime"]
-        if rctime == "null":
-            game_state[client]["hprechargetime"] = "null"  # hprechargetime, playervars needs to be loaded to python
-            rctime = 0
+        if client not in player_recharge_times:
+            player_recharge_times[client] = 0  # hprechargetime, playervars needs to be loaded to python
+        if client not in player_old_hps:
+            player_old_hps[client] = 500  # maxhp
+        if player_old_hps[client] > int(game_state[client]["hp"]):
+            player_recharge_times[client] = int(gametime)
+        rctime = player_recharge_times[client]
         rcplusdelay = rctime + delayhp
-        healdue = gametime >= rcplusdelay
-        if healdue:
-            playerhp = game_state[client]["hp"]
-            if playerhp != "null":
-                newhp = playerhp + 1
-                if newhp <= 500:  # maxhp, config vars need to be loaded into python
-                    game_state[client]["hp"] = newhp
+        if gametime >= rcplusdelay:
+            newhp = int(game_state[client]["hp"]) + 1
+            if newhp <= 500:  # maxhp, config vars need to be loaded into python
+                game_state[client]["hp"] = newhp
+        player_old_hps[client] = int(game_state[client]["hp"])
+        print(player_old_hps)
+        print(player_recharge_times)
