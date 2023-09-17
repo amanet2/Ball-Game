1c0d51a6714e228bcf7f94446e9a806c1f69c8db
amanet2 <tonyjmstallion@gmail.com>
2023-09-17 01:04:17 -0500

    bad commit, python cleaned up

--- python/custom/sv_checkgamestate.py
+++ python/custom/sv_checkgamestate.py
@@ -5,25 +5,19 @@ player_old_hps = {}
 
 
 def check_game_state(game_state):
+    gametime = time.time()  # gametime should be sent to python from java
     for client in game_state.keys():
-        game_state[client]["mov1"] = "1"
         # WIP: Check health recharge
-        # TODO: hprechargetime is set when damage occurs, so this prolly should be in java anyways
-        # TODO: OR everytime we check, we save the old hp and if the new check is less than old hp, we know we took dmg
-        gametime = time.time()  # gametime should be sent to python from java
-        delayhp = 3600
+        # TODO: save the old hp and if the new check is less than old hp, we know we took dmg
         if client not in player_recharge_times:
             player_recharge_times[client] = 0  # hprechargetime, playervars needs to be loaded to python
         if client not in player_old_hps:
             player_old_hps[client] = 500  # maxhp
         if player_old_hps[client] > int(game_state[client]["hp"]):
             player_recharge_times[client] = int(gametime)
-        rctime = player_recharge_times[client]
-        rcplusdelay = rctime + delayhp
+        rcplusdelay = player_recharge_times[client] + 3600  # delay time
         if gametime >= rcplusdelay:
             newhp = int(game_state[client]["hp"]) + 1
             if newhp <= 500:  # maxhp, config vars need to be loaded into python
                 game_state[client]["hp"] = newhp
         player_old_hps[client] = int(game_state[client]["hp"])
-        print(player_old_hps)
-        print(player_recharge_times)
