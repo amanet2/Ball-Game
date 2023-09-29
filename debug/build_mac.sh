e88ee1602079b4b2ea635ec92e96d515a2b17da5
Stallion <tonyjmstallion@gmail.com>
2023-09-28 22:10:52 -0500

    improved build script

--- debug/build_mac.sh
+++ debug/build_mac.sh
@@ -1,6 +1,11 @@
-out_dir="$(cd $(dirname "$1");pwd)"/../pkg
-"$(cd $(dirname "$1");pwd)"/../bin/jdk-18.jdk/Contents/Home/bin/javac -d $out_dir "$(cd $(dirname "$1");pwd)"/../src/*.java
+home_dir=~/Code/Ball-Game
+manifest_path=$home_dir/debug/MANIFEST.MF
+out_dir=$home_dir/pkg
+src_dir=$home_dir/src
+java_dir=$home_dir/bin/jdk-18.jdk/Contents/Home
+
+$java_dir/bin/javac -d $out_dir $src_dir/*.java
 cd $out_dir
-../bin/jdk-18.jdk/Contents/Home/bin/jar cmf ../debug/MANIFEST.MF BALL_GAME.jar *.class
+$java_dir/bin/jar cmf $manifest_path BALL_GAME.jar *.class
 rm *.class
 cd "$(cd $(dirname "$1");pwd)"
