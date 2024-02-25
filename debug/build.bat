23036f8aa883ad9ccb64829d1f15ea957dc04663
amanet2 <tonyjmstallion@gmail.com>
2024-02-25 04:05:18 -0600

    improve distrib script

--- debug/build.bat
+++ debug/build.bat
@@ -1,18 +1,22 @@
-set call_dir=%cd%
 set home_dir=C:\Code\Ball-Game
 set manifest_path=%home_dir%\debug\MANIFEST.MF
 set out_dir=%home_dir%\pkg
 set tmp_dir=%home_dir%\tmp
 set java_src=%home_dir%\src\*.java
 set java_bin=%home_dir%\bin\jdk-20.0.1\bin
+set jar_path=%out_dir%\BALL_GAME.jar
+
+REM clean up any existing files
+if exist %tmp_dir% rmdir /s /q %tmp_dir%
+if exist %jar_path% del %jar_path%
 
 REM create temp directory and compile java class files
-if exist %tmp_dir% rmdir /s /q %tmp_dir%
 mkdir %tmp_dir%
 %java_bin%\javac -d %tmp_dir% %java_src%
 
 REM generate jar file
-%java_bin%\jar cmf %manifest_path% %out_dir%\BALL_GAME.jar -C %tmp_dir% .
+REM note that the -C file for full path to .class files only works with the value "."
+%java_bin%\jar cmf %manifest_path% %jar_path% -C %tmp_dir% .
 
 REM cleanup temp files
 rmdir /s /q %tmp_dir%
--- debug/distrib.bat
+++ debug/distrib.bat
@@ -1,29 +1,30 @@
-set call_dir=%cd%
+REM run build process to generate jar
+set build_script=C:\Code\Ball-Game\debug\build.bat
+call %build_script%
+
+REM we have to set our variables here or they will be overwritten by previously called script
 set home_dir=C:\Code\Ball-Game
 set out_dir=C:\Code\pkg_ballmaster
-set manifest_path=%home_dir%\debug\MANIFEST.MF
+set out_zip=%out_dir%.zip
 set pkg_prefix=pkg
 set pkg_dir=%home_dir%\%pkg_prefix%
-set src_dir=%home_dir%\src
 set java_prefix=bin\jdk-20.0.1
 set java_dir=%home_dir%\%java_prefix%
 
-REM build jar
-%java_dir%\bin\javac -d %pkg_dir% %src_dir%\*.java
-cd %pkg_dir%
-%java_dir%\bin\jar cmf %manifest_path% BALL_GAME.jar *.class
-del *.class
-
-REM create archive
+REM cleanup existing files
+if exist %out_zip% del %out_zip%
 if exist %out_dir% rmdir /s /q %out_dir%
+
+REM copy files to output dir
 mkdir %out_dir%
 xcopy /y /e /i %java_dir% %out_dir%\%java_prefix%
 xcopy /y /e /i %pkg_dir% %out_dir%\%pkg_prefix%
 xcopy /y %home_dir%\Readme.txt %out_dir%
 xcopy /y %home_dir%\ballmaster.exe %out_dir%
 xcopy /y %home_dir%\ballmaster_editor.exe %out_dir%
-if exist %out_dir%.zip del %out_dir%.zip
-powershell.exe Compress-Archive -Path %out_dir%\* -DestinationPath %out_dir%.zip
 
-REM return to calling dir
-cd %call_dir%
+REM zip up output
+powershell.exe Compress-Archive -Path %out_dir%\* -DestinationPath %out_zip%
+
+REM cleanup temp files
+REM rmdir /s /q %out_dir%
