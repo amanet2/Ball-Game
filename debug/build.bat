99ec0d87bf7cd7942958b97af34ceafb12f7c863
amanet2 <tonyjmstallion@gmail.com>
2024-02-25 03:36:16 -0600

    improve build script with full paths

--- debug/build.bat
+++ debug/build.bat
@@ -2,11 +2,17 @@ set call_dir=%cd%
 set home_dir=C:\Code\Ball-Game
 set manifest_path=%home_dir%\debug\MANIFEST.MF
 set out_dir=%home_dir%\pkg
-set src_dir=%home_dir%\src
-set java_dir=%home_dir%\bin\jdk-20.0.1
+set tmp_dir=%home_dir%\tmp
+set java_src=%home_dir%\src\*.java
+set java_bin=%home_dir%\bin\jdk-20.0.1\bin
 
-%java_dir%\bin\javac -d %out_dir% %src_dir%\*.java
-cd %out_dir%
-%java_dir%\bin\jar cmf %manifest_path% BALL_GAME.jar *.class
-del *.class
-cd %call_dir%
+REM create temp directory and compile java class files
+if exist %tmp_dir% rmdir /s /q %tmp_dir%
+mkdir %tmp_dir%
+%java_bin%\javac -d %tmp_dir% %java_src%
+
+REM generate jar file
+%java_bin%\jar cmf %manifest_path% %out_dir%\BALL_GAME.jar -C %tmp_dir% .
+
+REM cleanup temp files
+rmdir /s /q %tmp_dir%
