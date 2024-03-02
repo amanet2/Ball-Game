39b4d885897abdff1fabf7293478bb50f2f17802
amanet2 <tonyjmstallion@gmail.com>
2024-03-02 05:01:02 -0600

    improve mapmaker

--- src/gCamera.java
+++ src/gCamera.java
@@ -1,10 +1,12 @@
 public class gCamera {
 	static int[] move = {0, 0, 0, 0};
 	static int[] coords = {0, 0};
-	private static final int velocity = 8;
+	private static final int velocity = 32;
 
 	public static void updatePosition() {
-		coords[0] += (velocity * move[3] - velocity * move[2]);
-		coords[1] += (velocity * move[1] - velocity * move[0]);
+		double mod = (double)sSettings.ratesimulation/(double)sSettings.rateShell;
+
+		coords[0] += (velocity * mod * move[3] - velocity * mod * move[2]);
+		coords[1] += (velocity * mod * move[1] - velocity * mod * move[0]);
 	}
 }
