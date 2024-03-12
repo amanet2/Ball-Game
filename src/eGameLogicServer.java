f42b0004cef21d1f9eff73b8408293cb7e4fbee8
amanet2 <tonyjmstallion@gmail.com>
2024-03-11 20:42:21 -0500

    attempt bug fix for client cmds rcv

--- src/eGameLogicServer.java
+++ src/eGameLogicServer.java
@@ -167,6 +167,7 @@ public class eGameLogicServer extends eGameLogicAdapter {
             }
             catch(Exception cre) {
                 cre.printStackTrace();
+                clientNetCmdMap.get(id).clear();
             }
         }
     }
