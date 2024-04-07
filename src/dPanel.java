4528d91baa873167033f419dc74c2f860342cd66
amanet2 <tonyjmstallion@gmail.com>
2024-04-06 22:44:44 -0500

    more prefabs

--- src/dPanel.java
+++ src/dPanel.java
@@ -1,7 +1,7 @@
 import javax.swing.JPanel;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.GridBagLayout;
+import java.awt.*;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * JPanel
@@ -21,16 +21,16 @@ public class dPanel extends JPanel {
         g.dispose();
     }
 
-    public void drawFrameUI(Graphics2D g2, long gameTimeMillis) {
+    private void drawFrameUI(Graphics2D g2, long gameTimeMillis) {
         dScreenFX.drawScreenFX(g2);
         dScreenMessages.displayScreenMessages(g2, gameTimeMillis);
         if(!sSettings.inplay && sSettings.show_mapmaker_ui && sSettings.clientMapLoaded) {
-            dThings.drawMapmakerPreviewBlockFloors(g2, xMain.shellLogic.clientPreviewScene);
-            dThings.drawBlockTopCubesPreview(g2);
+            drawMapmakerPreviewBlockFloors(g2, xMain.shellLogic.clientPreviewScene);
+            drawBlockTopCubesPreview(g2);
         }
     }
 
-    public void drawFrame(Graphics2D g2) {
+    private void drawFrame(Graphics2D g2) {
         if(!sSettings.clientMapLoaded) // comment out for no loading screens
             return;
         g2.translate(sSettings.width / 2, sSettings.height / 2);
@@ -43,8 +43,8 @@ public class dPanel extends JPanel {
         );
         g2.translate(-gCamera.coords[0], -gCamera.coords[1]);
         synchronized (scene.objectMaps) {
-            dThings.drawBlockFloors(g2, scene);
-            dThings.drawBlockWallsAndPlayers(g2, scene);
+            drawBlockFloors(g2, scene);
+            drawBlockWallsAndPlayers(g2, scene);
             dHUD.drawMapmakerOverlay(g2, scene);
             dHUD.drawBulletsAndAnimations(g2, scene);
             dHUD.drawWaypoints(g2, scene);
@@ -53,6 +53,61 @@ public class dPanel extends JPanel {
             dHUD.drawPlayerNames(g2);
             if (sSettings.show_mapmaker_ui)
                 dHUD.drawSelectionBoxes(g2);
+        }
+    }
+
+    private void drawBlockWallsAndPlayers(Graphics2D g2, gScene scene) {
+        Queue<gThing> visualQueue = scene.getWallsAndPlayersSortedByCoordY();
+        while(visualQueue.size() > 0) {
+            gThing thing = visualQueue.remove();
+            if(thing.type.equals("THING_PLAYER"))
+                ((gPlayer) thing).draw(g2);
+            else if(thing.type.startsWith("ITEM_"))
+                ((gItem) thing).draw(g2);
+            else if(thing.type.contains("CUBE"))
+                ((gBlockCube) thing).draw(g2);
+        }
+        ConcurrentHashMap<String, gThing> floorMap = scene.getThingMap("BLOCK_FLOOR");
+        for(String tag : floorMap.keySet()) {
+            gThing floor = floorMap.get(tag);
+            //flashlight
+            if(xMain.shellLogic.getUserPlayer() != null) {
+                int aimerx = eUtils.unscaleInt(xMain.shellLogic.getMouseCoordinates()[0]);
+                int aimery = eUtils.unscaleInt(xMain.shellLogic.getMouseCoordinates()[1]);
+                int snapX = aimerx + (int) gCamera.coords[0];
+                int snapY = aimery + (int) gCamera.coords[1];
+                int setw = sSettings.height;
+                RadialGradientPaint df = new RadialGradientPaint(new Point(snapX, snapY), setw/2,
+                        new float[]{0f, 1f}, new Color[]{new Color(0,0,0,0), gColors.getColorFromName("clrw_ambientshading")}
+                );
+                g2.setPaint(df);
+                g2.fillRect(floor.coords[0], floor.coords[1], floor.dims[0], floor.dims[1]);
+            }
+            else {
+                g2.setColor(gColors.getColorFromName("clrw_ambientshading"));
+                g2.fillRect(floor.coords[0], floor.coords[1], floor.dims[0], floor.dims[1]);
+            }
+        }
+    }
+
+    private void drawBlockFloors(Graphics2D g2, gScene scene) {
+        ConcurrentHashMap<String, gThing> floorMap = scene.getThingMap("BLOCK_FLOOR");
+        for(String tag : floorMap.keySet()) {
+            ((gBlockFloor) floorMap.get(tag)).draw(g2);
+        }
+    }
+
+    private void drawMapmakerPreviewBlockFloors(Graphics2D g2, gScene scene) {
+        ConcurrentHashMap<String, gThing> floorMap = scene.getThingMap("BLOCK_FLOOR");
+        for(String tag : floorMap.keySet()) {
+            ((gBlockFloor) floorMap.get(tag)).drawPreview(g2);
+        }
+    }
+
+    private void drawBlockTopCubesPreview(Graphics2D g2) {
+        ConcurrentHashMap<String, gThing> squareMap = xMain.shellLogic.clientPreviewScene.getThingMap("BLOCK_CUBE");
+        for(String tag : squareMap.keySet()) {
+            ((gBlockCube) squareMap.get(tag)).drawPreview(g2);
         }
     }
 
