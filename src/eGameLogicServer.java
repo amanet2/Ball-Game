e2586fb6ba9efc7722676834a97811d917120894
Stallion <tonyjmstallion@gmail.com>
2025-03-14 20:11:41 -0500

    cut down on api requests since we will now use the checkin interval

--- src/eGameLogicServer.java
+++ src/eGameLogicServer.java
@@ -216,16 +216,17 @@ public class eGameLogicServer extends eGameLogicAdapter {
         clientNetCmdBatchMap.remove(id);
         gScoreboard.scoresMap.remove(id);
         xMain.shellLogic.console.ex("deleteplayer " + id);
-        try {
-            URL updatemyip = new URL(String.format(sSettings.serverBrowserBase + "/updatemyplayercount?players=%d",
-                    masterStateMap.keys().size()
-            ));
-            BufferedReader res = new BufferedReader(new InputStreamReader(updatemyip.openStream()));
-            String resl = res.readLine(); //you get the IP as a String
-            System.out.println("RESPONSE FROM FASTAPI SERVER: " + resl);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        // if we check in every 15 seconds, we dont need to update on player join/leave
+//        try {
+//            URL updatemyip = new URL(String.format(sSettings.serverBrowserBase + "/updatemyplayercount?players=%d",
+//                    masterStateMap.keys().size()
+//            ));
+//            BufferedReader res = new BufferedReader(new InputStreamReader(updatemyip.openStream()));
+//            String resl = res.readLine(); //you get the IP as a String
+//            System.out.println("RESPONSE FROM FASTAPI SERVER: " + resl);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
     }
 
     private void sendMapAndRespawn(String id) {
@@ -235,16 +236,17 @@ public class eGameLogicServer extends eGameLogicAdapter {
     }
 
     public void handleJoin(String id) {
-        try {
-            URL updatemyip = new URL(String.format(sSettings.serverBrowserBase + "/updatemyplayercount?players=%d",
-                    masterStateMap.keys().size() + 1
-            ));
-            BufferedReader res = new BufferedReader(new InputStreamReader(updatemyip.openStream()));
-            String resl = res.readLine(); //you get the IP as a String
-            System.out.println("RESPONSE FROM FASTAPI SERVER: " + resl);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        // if we check in every 15 seconds, we dont need to update on player join/leave
+//        try {
+//            URL updatemyip = new URL(String.format(sSettings.serverBrowserBase + "/updatemyplayercount?players=%d",
+//                    masterStateMap.keys().size() + 1
+//            ));
+//            BufferedReader res = new BufferedReader(new InputStreamReader(updatemyip.openStream()));
+//            String resl = res.readLine(); //you get the IP as a String
+//            System.out.println("RESPONSE FROM FASTAPI SERVER: " + resl);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
         if(!id.startsWith("bot"))
             masterStateMap.put(id, new nStateBallGame());
         else {
